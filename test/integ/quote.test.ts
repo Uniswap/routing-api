f22a893c47313afddf963b777744a77db6e8be7d
Sara Reynolds <snreynolds2506@gmail.com>
2021-12-23 16:48:16 -0800

    increase timeout and include ratio tests

--- test/integ/quote.test.ts
+++ test/integ/quote.test.ts
@@ -75,11 +75,11 @@ const checkQuoteToken = (
 
 const SWAP_ROUTER_V2 = '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45'
 
-describe.only('quote', function () {
+describe('quote', function () {
   // Help with test flakiness by retrying.
   this.retries(2)
 
-  this.timeout(10000)
+  this.timeout(500000)
 
   let alice: SignerWithAddress
   let block: number
