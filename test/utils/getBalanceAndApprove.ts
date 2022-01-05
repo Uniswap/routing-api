9d9cccc3b0f1a02c6e66492edec27615aa911fb9
Emily Williams <emag3m@gmail.com>
2022-01-05 14:08:51 -0500

    small function cleanup

--- test/utils/getBalanceAndApprove.ts
+++ test/utils/getBalanceAndApprove.ts
@@ -14,14 +14,13 @@ export const getBalance = async (alice: SignerWithAddress, currency: Currency): 
   return CurrencyAmount.fromRawAmount(currency, (await aliceTokenIn.balanceOf(alice.address)).toString())
 }
 
-// TODO clean this up if possible.
 export const getBalanceOfAddress = async (
   alice: SignerWithAddress,
   address: string,
-  currency: Currency
-): Promise<CurrencyAmount<Currency>> => {
-  // TODO: tokens / WETH only.
-  const token: Erc20 = Erc20__factory.connect(currency.wrapped.address, alice)
+  currency: Token
+): Promise<CurrencyAmount<Token>> => {
+  // tokens / WETH only.
+  const token: Erc20 = Erc20__factory.connect(currency.address, alice)
 
   return CurrencyAmount.fromRawAmount(currency, (await token.balanceOf(address)).toString())
 }
