98439fa9697617da7156cac485ae29747291447f
Raghava Pamula <raghavapamula@UNISWAP-MAC-017.lan>
2022-06-06 14:05:46 -0400

    bump lambda versions

--- bin/stacks/routing-lambda-stack.ts
+++ bin/stacks/routing-lambda-stack.ts
@@ -66,7 +66,7 @@ export class RoutingLambdaStack extends cdk.NestedStack {
       },
       description: 'Routing Lambda',
       environment: {
-        VERSION: '2',
+        VERSION: '3',
         NODE_OPTIONS: '--enable-source-maps',
         POOL_CACHE_BUCKET: poolCacheBucket.bucketName,
         POOL_CACHE_BUCKET_2: poolCacheBucket2.bucketName,
@@ -98,7 +98,7 @@ export class RoutingLambdaStack extends cdk.NestedStack {
       },
       description: 'Route to Ratio Lambda',
       environment: {
-        VERSION: '2',
+        VERSION: '3',
         NODE_OPTIONS: '--enable-source-maps',
         POOL_CACHE_BUCKET: poolCacheBucket.bucketName,
         POOL_CACHE_BUCKET_2: poolCacheBucket2.bucketName,
