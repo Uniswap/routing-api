767c2cb94875b36e9d60aeaf6573c4094bfa6bd4
Raghava Pamula <raghava.pamula@uniswap.org>
2022-07-13 15:30:15 -0400

    tests

--- api_test.py
+++ api_test.py
@@ -1,4 +1,9 @@
-ROUTE_AND_SIMULATE = 'https://vc02kvm3ei.execute-api.us-east-1.amazonaws.com/prod/quote?tokenInAddress=0x6B175474E89094C44Da98b954EedeAC495271d0F&tokenInChainId=1&tokenOutAddress=0x1f9840a85d5af5bf1d1762f925bdaddc4201f984&tokenOutChainId=1&amount=1&type=exactIn&recipient=0x63946551716781c32f0269f87dc08521818b6292&slippageTolerance=5&deadline=360&simulate=true'
+tokenInAddress = '0x2ba592F78dB6436527729929AAf6c908497cB200'
+tokenOutAddress = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984'
+chainId = 1
+recipient = 0x63946551716781c32f0269f87dc08521818b6292
+
+ROUTE_AND_SIMULATE = 'https://vc02kvm3ei.execute-api.us-east-1.amazonaws.com/prod/quote?tokenInAddress=0x6B175474E89094C44Da98b954EedeAC495271d0F&tokenInChainId={}&tokenOutAddress=0x1f9840a85d5af5bf1d1762f925bdaddc4201f984&tokenOutChainId=1&amount=1&type=exactIn&recipient=0x63946551716781c32f0269f87dc08521818b6292&slippageTolerance=5&deadline=360&simulate=true'
 
 ROUTE = 'https://vc02kvm3ei.execute-api.us-east-1.amazonaws.com/prod/quote?tokenInAddress=0x6B175474E89094C44Da98b954EedeAC495271d0F&tokenInChainId=1&tokenOutAddress=0x1f9840a85d5af5bf1d1762f925bdaddc4201f984&tokenOutChainId=1&amount=1&type=exactIn&recipient=0x63946551716781c32f0269f87dc08521818b6292&slippageTolerance=5&deadline=360&simulate=false'
 
