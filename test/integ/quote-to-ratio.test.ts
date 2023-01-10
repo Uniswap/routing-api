2d3a4a784951d4182f6e579a7895a8cf72aff818
pote.eth <81638931+willpote@users.noreply.github.com>
2023-01-10 16:15:01 -0500

    Add arb goerli (#113)
    * Add arb goerli

--- test/integ/quote-to-ratio.test.ts
+++ test/integ/quote-to-ratio.test.ts
@@ -712,6 +712,7 @@ describe('quote-to-ratio', async function () {
     [ChainId.OPTIMISTIC_KOVAN]: USDC_ON(ChainId.OPTIMISTIC_KOVAN),
     [ChainId.ARBITRUM_ONE]: USDC_ON(ChainId.ARBITRUM_ONE),
     [ChainId.ARBITRUM_RINKEBY]: USDC_ON(ChainId.ARBITRUM_RINKEBY),
+    [ChainId.ARBITRUM_GOERLI]: null,
     [ChainId.POLYGON]: USDC_ON(ChainId.POLYGON),
     [ChainId.POLYGON_MUMBAI]: USDC_ON(ChainId.POLYGON_MUMBAI),
     [ChainId.CELO]: CUSD_CELO,
@@ -736,6 +737,7 @@ describe('quote-to-ratio', async function () {
     [ChainId.CELO_ALFAJORES]: CEUR_CELO_ALFAJORES,
     [ChainId.MOONBEAM]: null,
     [ChainId.GNOSIS]: null,
+    [ChainId.ARBITRUM_GOERLI]: null,
   }
 
   for (const chain of _.filter(
@@ -747,6 +749,7 @@ describe('quote-to-ratio', async function () {
       c != ChainId.OPTIMISTIC_KOVAN &&
       c != ChainId.POLYGON_MUMBAI &&
       c != ChainId.ARBITRUM_RINKEBY &&
+      c != ChainId.ARBITRUM_GOERLI &&
       c != ChainId.GÖRLI &&
       c != ChainId.MOONBEAM &&
       c != ChainId.GNOSIS &&
@@ -758,7 +761,7 @@ describe('quote-to-ratio', async function () {
     const erc1 = TEST_ERC20_1[chain]
     const erc2 = TEST_ERC20_2[chain]
 
-    // This is for Gnosis and Moonbeam which we don't have RPC Providers yet
+    // This is for Gnosis, Moonbeam, and Arbitrum Goerli which we don't have RPC Providers yet
     if (erc1 == null || erc2 == null) continue
 
     describe(`${ID_TO_NETWORK_NAME(chain)} 2xx`, function () {
