8e1e6762f4c6eabcd3da650cd72487779700e67d
Eric Zhong <eric.zhong@uniswap.org>
2023-03-03 17:57:44 -0600

    Bump routing lambda stack version 3 -> 4

--- bin/stacks/routing-lambda-stack.ts
+++ bin/stacks/routing-lambda-stack.ts
@@ -72,7 +72,7 @@ export class RoutingLambdaStack extends cdk.NestedStack {
       },
       description: 'Routing Lambda',
       environment: {
-        VERSION: '3',
+        VERSION: '4',
         NODE_OPTIONS: '--enable-source-maps',
         POOL_CACHE_BUCKET: poolCacheBucket.bucketName,
         POOL_CACHE_BUCKET_2: poolCacheBucket2.bucketName,
@@ -107,7 +107,7 @@ export class RoutingLambdaStack extends cdk.NestedStack {
       },
       description: 'Route to Ratio Lambda',
       environment: {
-        VERSION: '3',
+        VERSION: '4',
         NODE_OPTIONS: '--enable-source-maps',
         POOL_CACHE_BUCKET: poolCacheBucket.bucketName,
         POOL_CACHE_BUCKET_2: poolCacheBucket2.bucketName,
