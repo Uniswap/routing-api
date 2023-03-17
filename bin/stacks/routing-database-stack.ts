abc64071c8db248d4f8fb4ef82b25dcb3998f566
Miguel Cervera <miguel.cervera@uniswap.org>
2023-03-16 17:30:21 -0700

    Create DynamoDB Table for storing CachedRoutes

---
+++ bin/stacks/routing-database-stack.ts
@@ -0,0 +1,23 @@
+import * as cdk from 'aws-cdk-lib'
+import * as aws_dynamodb from 'aws-cdk-lib/aws-dynamodb'
+import { AttributeType, BillingMode } from 'aws-cdk-lib/aws-dynamodb'
+import { Construct } from 'constructs'
+
+export interface RoutingDatabaseStackProps extends cdk.NestedStackProps {
+  stage: string
+}
+
+export class RoutingDatabaseStack extends cdk.NestedStack {
+  public readonly cachedRoutesDynamoDb: aws_dynamodb.Table
+
+  constructor(scope: Construct, name: string, props: RoutingDatabaseStackProps) {
+    super(scope, name, props)
+
+    this.cachedRoutesDynamoDb = new aws_dynamodb.Table(this, 'RouteCachingDB', {
+      partitionKey: { name: 'pairTradeTypeChainId', type: AttributeType.STRING },
+      sortKey: { name: 'protocolsAmountBlockNumber', type: AttributeType.STRING },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      timeToLiveAttribute: 'ttl'
+    });
+  }
+}
