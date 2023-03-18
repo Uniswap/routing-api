55049d76a6926d77774306e80723342ccfe37b74
Mark Toda <toda.mark@gmail.com>
2023-03-17 18:15:30 -0700

    feat: bump version to 5

--- bin/stacks/routing-lambda-stack.ts
+++ bin/stacks/routing-lambda-stack.ts
@@ -72,7 +72,7 @@ export class RoutingLambdaStack extends cdk.NestedStack {
       },
       description: 'Routing Lambda',
       environment: {
-        VERSION: '4',
+        VERSION: '5',
         NODE_OPTIONS: '--enable-source-maps',
         POOL_CACHE_BUCKET: poolCacheBucket.bucketName,
         POOL_CACHE_BUCKET_2: poolCacheBucket2.bucketName,
