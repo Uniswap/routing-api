a32d656eca57772d111068d922375024a610953b
Miguel Cervera <miguel.cervera@uniswap.org>
2023-03-27 13:16:42 -0700

    tests for strategies

--- lib/handlers/router-entities/route-caching/model/cached-routes-strategy.ts
+++ lib/handlers/router-entities/route-caching/model/cached-routes-strategy.ts
@@ -1,38 +1,31 @@
 import { Currency, CurrencyAmount } from '@uniswap/sdk-core'
 import { CachedRoutesParameters } from './cached-routes-parameters'
+import { log } from '@uniswap/smart-order-router'
 
 export class CachedRoutesStrategy {
-  public readonly cachingParameters: Map<number, CachedRoutesParameters>
+  private cachingParameters: Map<number, CachedRoutesParameters>
   private buckets: number[]
-  private cachedCachingParameters: Map<CurrencyAmount<Currency>, CachedRoutesParameters> = new Map()
 
   constructor(cachedRoutesParameters: CachedRoutesParameters[]) {
-    this.buckets = cachedRoutesParameters.map((params) => params.bucket).sort()
+    this.buckets = cachedRoutesParameters.map((params) => params.bucket).sort((a, b) => a-b)
     this.cachingParameters = new Map(cachedRoutesParameters.map((params) => [params.bucket, params]))
   }
 
   public getCachingParameters(amount: CurrencyAmount<Currency>): CachedRoutesParameters | undefined {
-    const fromMemoryCache = this.cachedCachingParameters.get(amount)
+    // Find the first bucket which is greater or equal than the amount.
+    // If no bucket is found it means it's not supposed to be cached.
+    // e.g. if buckets = [10, 50, 100, 500, 1000] and amount = 0.10, then bucket = 10
+    // e.g.2. if amount = 501, then bucket = 1000. If amount = 1001 then bucket = undefined
+    const bucket = this.buckets.find((bucket) => {
+      const bucketCurrency = CurrencyAmount.fromRawAmount(amount.currency, bucket * (10 ** amount.currency.decimals))
+      log.info(`Looking for ${amount.currency.symbol} bucket for ${amount.toExact()}. Currently checking ${bucketCurrency.toExact()}`)
+      return amount.lessThan(bucketCurrency) || amount.equalTo(bucketCurrency)
+    })
 
-    if (fromMemoryCache) {
-      return fromMemoryCache
-    } else {
-      // Find the first bucket which is greater or equal than the amount.
-      // If no bucket is found it means it's not supposed to be cached.
-      // e.g. if buckets = [10, 50, 100, 500, 1000] and amount = 0.10, then bucket = 10
-      // e.g.2. if amount = 501, then bucket = 1000. If amount = 1001 then bucket = undefined
-      const bucket = this.buckets.find((bucket) => amount.lessThan(bucket) || amount.equalTo(bucket))
-
-      if (bucket) {
-        const cachingParameter = this.cachingParameters.get(bucket)
-        if (cachingParameter) {
-          this.cachedCachingParameters.set(amount, cachingParameter)
-
-          return cachingParameter
-        }
-      }
-
-      return undefined
+    if (bucket) {
+      return this.cachingParameters.get(bucket)
     }
+
+    return undefined
   }
 }
