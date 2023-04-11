0cc2202ced87406ebefe5b61f757131495fd1aa2
Miguel Cervera <miguel.cervera@uniswap.org>
2023-04-11 09:48:24 -0700

    Add originalAmount to dashboard

--- lib/dashboards/cached-routes-widgets-factory.ts
+++ lib/dashboards/cached-routes-widgets-factory.ts
@@ -218,7 +218,7 @@ export class CachedRoutesWidgetsFactory implements WidgetsFactory {
         properties: {
           view: 'table',
           query: `SOURCE '/aws/lambda/${this.lambdaName}'
-            | fields @timestamp, pair, quoteGasAdjustedDiff as diffOf${tokenOut}, amount as amountOf${tokenIn}, quoteGasAdjustedDiff * (amount/quoteGasAdjustedFromChain) as diffIn${tokenIn}Terms, diffIn${tokenIn}Terms / amount * 100 as misquotePercent
+            | fields @timestamp, pair, quoteGasAdjustedDiff as diffOf${tokenOut}, amount as amountOf${tokenIn}, quoteGasAdjustedDiff * (amount/quoteGasAdjustedFromChain) as diffIn${tokenIn}Terms, diffIn${tokenIn}Terms / amount * 100 as misquotePercent, originalAmount
             | filter msg like 'Comparing quotes between Chain and Cache' and pair like /${escapedPairTradeTypeChainId}/ and quoteGasAdjustedDiff != 0 
             | sort misquotePercent desc`,
           region: this.region,
