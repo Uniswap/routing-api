5ac5fa405e506fbd935838c3f5731b90668762f8
Miguel Cervera <miguel.cervera@uniswap.org>
2023-04-11 13:44:31 -0700

    Add new heuristics for deciding when to add a route to cache

--- lib/handlers/router-entities/route-caching/model/cached-routes-bucket.ts
+++ lib/handlers/router-entities/route-caching/model/cached-routes-bucket.ts
@@ -14,16 +14,23 @@ interface CachedRoutesBucketsArgs {
    * The CacheMode associated to this bucket. Setting it to `Livemode` will enable caching the route for this bucket
    */
   cacheMode: CacheMode
+  /**
+   * Defines the max number of splits allowed for a route to be cached. A value of 0 indicates that any splits are allowed
+   * A value of 1 indicates that at most there can only be 1 split in the route in order to be cached.
+   */
+  maxSplits?: number
 }
 
 export class CachedRoutesBucket {
   public readonly bucket: number
   public readonly blocksToLive: number
   public readonly cacheMode: CacheMode
+  public readonly maxSplits: number
 
-  constructor({ bucket, blocksToLive = 1, cacheMode }: CachedRoutesBucketsArgs) {
+  constructor({ bucket, blocksToLive = 1, cacheMode, maxSplits = 0 }: CachedRoutesBucketsArgs) {
     this.bucket = bucket
     this.blocksToLive = blocksToLive // by default this value is 1, which means it's only cached in the current block.
     this.cacheMode = cacheMode
+    this.maxSplits = maxSplits // by default this value is 0, which means that any number of splits are allowed
   }
 }
