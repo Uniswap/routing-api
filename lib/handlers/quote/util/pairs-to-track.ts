a71ea7599201cf02f03ce43e3aef9d1112f3cde4
Lint Action <lint-action@samuelmeuli.com>
2023-04-19 21:53:56 +0000

    Fix code style issues with Prettier

--- lib/handlers/quote/util/pairs-to-track.ts
+++ lib/handlers/quote/util/pairs-to-track.ts
@@ -5,7 +5,10 @@ export const PAIRS_TO_TRACK: Map<ChainId, Map<TradeType, string[]>> = new Map([
   [
     ChainId.MAINNET,
     new Map([
-      [TradeType.EXACT_INPUT, ['WETH/USDC', 'USDC/WETH', 'USDT/WETH', 'WETH/USDT', 'WETH/*', 'USDC/*', 'USDT/*', 'DAI/*', 'WBTC/*']],
+      [
+        TradeType.EXACT_INPUT,
+        ['WETH/USDC', 'USDC/WETH', 'USDT/WETH', 'WETH/USDT', 'WETH/*', 'USDC/*', 'USDT/*', 'DAI/*', 'WBTC/*'],
+      ],
       [TradeType.EXACT_OUTPUT, ['USDC/WETH', '*/WETH', '*/USDC', '*/USDT', '*/DAI']],
     ]),
   ],
