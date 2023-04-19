eb8469259e272465e5bdacc28b54e6182c57e39a
Miguel Cervera <miguel.cervera@uniswap.org>
2023-04-19 15:00:20 -0700

    Add more pairs to track (#167)
    * Add more pairs to track
    
    * Fix code style issues with Prettier
    
    ---------
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

--- lib/handlers/quote/util/pairs-to-track.ts
+++ lib/handlers/quote/util/pairs-to-track.ts
@@ -5,8 +5,11 @@ export const PAIRS_TO_TRACK: Map<ChainId, Map<TradeType, string[]>> = new Map([
   [
     ChainId.MAINNET,
     new Map([
-      [TradeType.EXACT_INPUT, ['WETH/USDC', 'USDC/WETH', 'USDT/WETH', 'WETH/USDT', 'WETH/*']],
-      [TradeType.EXACT_OUTPUT, ['USDC/WETH']],
+      [
+        TradeType.EXACT_INPUT,
+        ['WETH/USDC', 'USDC/WETH', 'USDT/WETH', 'WETH/USDT', 'WETH/*', 'USDC/*', 'USDT/*', 'DAI/*', 'WBTC/*'],
+      ],
+      [TradeType.EXACT_OUTPUT, ['USDC/WETH', '*/WETH', '*/USDC', '*/USDT', '*/DAI']],
     ]),
   ],
   [ChainId.OPTIMISM, new Map([[TradeType.EXACT_INPUT, ['WETH/USDC', 'USDC/WETH']]])],
