86ec50f82e587e553c8576cbe9e47e4dbb441e8a
Miguel Cervera <miguel.cervera@uniswap.org>
2023-04-25 13:27:22 -0700

    Include metrics for misquotes in dashboard (#172)
    * Update Dashboard to include misquote rates
    
    * Title of misquote graphs
    
    * Fix code style issues with Prettier
    
    ---------
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

--- lib/dashboards/cached-routes-widgets-factory.ts
+++ lib/dashboards/cached-routes-widgets-factory.ts
@@ -55,7 +55,30 @@ export class CachedRoutesWidgetsFactory implements WidgetsFactory {
       },
       {
         type: 'metric',
-        width: 24,
+        width: 12,
+        height: 7,
+        properties: {
+          view: 'timeSeries',
+          stacked: false,
+          metrics: [
+            [{ expression: 'SUM(METRICS())', label: 'Requests', id: 'e1' }],
+            [this.namespace, 'GetCachedRoute_hit_livemode', 'Service', 'RoutingAPI', { label: 'Cache Hit', id: 'm1' }],
+            ['.', 'GetCachedRoute_miss_livemode', '.', '.', { label: 'Cache Miss', id: 'm2' }],
+          ],
+          region: this.region,
+          title: 'Cache Hit, Miss and Total requests of Cachemode.Livemode',
+          period: 300,
+          stat: 'Sum',
+          yAxis: {
+            left: {
+              min: 0,
+            },
+          },
+        },
+      },
+      {
+        type: 'metric',
+        width: 12,
         height: 7,
         properties: {
           view: 'timeSeries',
@@ -87,7 +110,36 @@ export class CachedRoutesWidgetsFactory implements WidgetsFactory {
       },
       {
         type: 'metric',
-        width: 24,
+        width: 12,
+        height: 7,
+        properties: {
+          view: 'timeSeries',
+          stacked: false,
+          metrics: [
+            [{ expression: 'SUM(METRICS())', label: 'Requests', id: 'e1' }],
+            [
+              this.namespace,
+              'GetCachedRoute_hit_tapcompare',
+              'Service',
+              'RoutingAPI',
+              { label: 'Cache Hit', id: 'm1' },
+            ],
+            ['.', 'GetCachedRoute_miss_tapcompare', '.', '.', { label: 'Cache Miss', id: 'm2' }],
+          ],
+          region: this.region,
+          title: 'Cache Hit, Miss and Total requests of Cachemode.Tapcompare',
+          period: 300,
+          stat: 'Sum',
+          yAxis: {
+            left: {
+              min: 0,
+            },
+          },
+        },
+      },
+      {
+        type: 'metric',
+        width: 12,
         height: 7,
         properties: {
           view: 'timeSeries',
@@ -107,6 +159,69 @@ export class CachedRoutesWidgetsFactory implements WidgetsFactory {
           ],
           region: this.region,
           title: 'Cache Hit and Miss Rates of cachemode.Tapcompare',
+          period: 300,
+          stat: 'Sum',
+          yAxis: {
+            left: {
+              min: 0,
+              max: 100,
+            },
+          },
+        },
+      },
+      {
+        type: 'metric',
+        width: 12,
+        height: 7,
+        properties: {
+          view: 'timeSeries',
+          stacked: false,
+          metrics: [
+            [
+              this.namespace,
+              'TapcompareCachedRoute_quoteGasAdjustedDiffPercent',
+              'Service',
+              'RoutingAPI',
+              { label: 'Misquote' },
+            ],
+          ],
+          region: this.region,
+          title: 'Total number of Misquotes from Tapcompare',
+          period: 300,
+          stat: 'SampleCount',
+          yAxis: {
+            left: {
+              min: 0,
+            },
+          },
+        },
+      },
+      {
+        type: 'metric',
+        width: 12,
+        height: 7,
+        properties: {
+          view: 'timeSeries',
+          stacked: false,
+          metrics: [
+            [{ expression: 'm2/m1 * 100', label: 'Misquote Rate', id: 'e1' }],
+            [
+              this.namespace,
+              'GetCachedRoute_hit_tapcompare',
+              'Service',
+              'RoutingAPI',
+              { label: 'Cache Hit', id: 'm1', visible: false },
+            ],
+            [
+              '.',
+              'TapcompareCachedRoute_quoteGasAdjustedDiffPercent',
+              '.',
+              '.',
+              { label: 'Cache Miss', id: 'm2', stat: 'SampleCount', visible: false },
+            ],
+          ],
+          region: this.region,
+          title: 'Misquote rate from Tapcompare',
           period: 300,
           stat: 'Sum',
           yAxis: {
