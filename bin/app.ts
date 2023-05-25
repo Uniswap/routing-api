3efeb6f374e15d008633cb5683a01c959a5521ef
matteen <105068213+matteenm@users.noreply.github.com>
2023-05-25 17:38:44 -0400

    Revert "Revert "temp bump rate limit for beta" (#128)"
    This reverts commit 57817138d550e1fe9c375b18d6b05a613d872cf4.

--- bin/app.ts
+++ bin/app.ts
@@ -54,6 +54,7 @@ export class RoutingAPIStage extends Stage {
     const { url } = new RoutingAPIStack(this, 'RoutingAPI', {
       jsonRpcProviders,
       provisionedConcurrency,
+      throttlingOverride: stage == STAGE.BETA ? '3000' : undefined,
       ethGasStationInfoUrl,
       chatbotSNSArn,
       stage,
