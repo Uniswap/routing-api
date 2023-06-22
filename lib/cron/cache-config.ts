ff0d4848b8e35c180274af48541165bb92d93496
Miguel Cervera <miguel.cervera@uniswap.org>
2023-06-22 13:00:07 -0700

    Update CachePool params (#252)

--- lib/cron/cache-config.ts
+++ lib/cron/cache-config.ts
@@ -7,30 +7,35 @@ export const chainProtocols = [
     protocol: Protocol.V3,
     chainId: ChainId.MAINNET,
     timeout: 90000,
+    lambdaTimeoutInSeconds: 900,
     provider: new V3SubgraphProvider(ChainId.MAINNET, 3, 90000),
   },
   {
     protocol: Protocol.V3,
     chainId: ChainId.ARBITRUM_ONE,
     timeout: 90000,
+    lambdaTimeoutInSeconds: 900,
     provider: new V3SubgraphProvider(ChainId.ARBITRUM_ONE, 3, 90000),
   },
   {
     protocol: Protocol.V3,
     chainId: ChainId.POLYGON,
     timeout: 90000,
+    lambdaTimeoutInSeconds: 900,
     provider: new V3SubgraphProvider(ChainId.POLYGON, 3, 90000),
   },
   {
     protocol: Protocol.V3,
     chainId: ChainId.CELO,
     timeout: 90000,
+    lambdaTimeoutInSeconds: 900,
     provider: new V3SubgraphProvider(ChainId.CELO, 3, 90000),
   },
   {
     protocol: Protocol.V3,
     chainId: ChainId.BSC,
     timeout: 90000,
+    lambdaTimeoutInSeconds: 900,
     provider: new V3SubgraphProvider(ChainId.BSC, 3, 90000),
   },
   // Currently there is no working V3 subgraph for Optimism so we use a static provider.
@@ -39,7 +44,8 @@ export const chainProtocols = [
   {
     protocol: Protocol.V2,
     chainId: ChainId.MAINNET,
-    timeout: 840000,
-    provider: new V2SubgraphProvider(ChainId.MAINNET, 3, 900000, true, 250),
+    timeout: 1760000,
+    lambdaTimeoutInSeconds: 1800,
+    provider: new V2SubgraphProvider(ChainId.MAINNET, 3, 1800000, true, 500),
   },
 ]
