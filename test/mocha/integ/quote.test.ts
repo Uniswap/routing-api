6712d492c702f52e736f2ccdb02ea6d7ef024f22
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2023-08-09 10:53:22 -0700

    fix: unfilter base integ test (#279)
    * unfilter base integ test
    
    * add TODO with linear ticket

--- test/mocha/integ/quote.test.ts
+++ test/mocha/integ/quote.test.ts
@@ -1934,9 +1934,7 @@ describe('quote', function () {
       c != ChainId.ARBITRUM_GOERLI &&
       c != ChainId.CELO_ALFAJORES &&
       c != ChainId.GOERLI &&
-      c != ChainId.SEPOLIA &&
-      c != ChainId.BASE &&
-      c != ChainId.BASE_GOERLI
+      c != ChainId.SEPOLIA
   )) {
     for (const type of ['exactIn', 'exactOut']) {
       const erc1 = TEST_ERC20_1[chain]()
@@ -1996,10 +1994,14 @@ describe('quote', function () {
         })
         const native = NATIVE_CURRENCY[chain]
         it(`${native} -> erc20`, async () => {
+          // TODO ROUTE-64: Remove this once smart-order-router supports ETH native currency on BASE
+          // see https://uniswapteam.slack.com/archives/C021SU4PMR7/p1691593679108459?thread_ts=1691532336.742419&cid=C021SU4PMR7
+          const baseErc20 = chain == ChainId.BASE ? USDC_ON(ChainId.BASE) : erc2
+
           const quoteReq: QuoteQueryParams = {
             tokenInAddress: native,
             tokenInChainId: chain,
-            tokenOutAddress: erc2.address,
+            tokenOutAddress: baseErc20.address,
             tokenOutChainId: chain,
             amount: await getAmountFromToken(type, WNATIVE_ON(chain), erc2, '1'),
             type,
