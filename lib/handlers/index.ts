045003ff3092545fdb6c901a943f4790a703e031
Miguel Cervera <miguel.cervera@uniswap.org>
2023-09-12 18:43:04 -0700

    Log handler initializer (#366)
    * Log handler initializer
    
    * prettier

--- lib/handlers/index.ts
+++ lib/handlers/index.ts
@@ -1,9 +1,21 @@
 import { QuoteHandlerInjector } from './quote/injector'
 import { QuoteHandler } from './quote/quote'
+import { default as bunyan, default as Logger } from 'bunyan'
 
-const quoteInjectorPromise = new QuoteHandlerInjector('quoteInjector').build()
+const log: Logger = bunyan.createLogger({
+  name: 'Root',
+  serializers: bunyan.stdSerializers,
+  level: bunyan.INFO,
+})
 
-const quoteHandler = new QuoteHandler('quote', quoteInjectorPromise)
+let quoteHandler: QuoteHandler
+try {
+  const quoteInjectorPromise = new QuoteHandlerInjector('quoteInjector').build()
+  quoteHandler = new QuoteHandler('quote', quoteInjectorPromise)
+} catch (error) {
+  log.fatal({ error }, 'Fatal error')
+  throw error
+}
 
 module.exports = {
   quoteHandler: quoteHandler.handler,
