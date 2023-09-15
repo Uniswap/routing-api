b7910c7346f036d0b7798a1b91c080c6df4f75e4
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2023-09-14 17:37:17 -0700

    correct clock skew limited to aws token list provider only (#368)
    * correct clock skew limited to aws token list provider only
    
    * aws token-list provider max retries = 3/max attemps = 4

--- lib/handlers/router-entities/aws-token-list-provider.ts
+++ lib/handlers/router-entities/aws-token-list-provider.ts
@@ -18,7 +18,7 @@ export class AWSTokenListProvider extends CachingTokenListProvider {
     bucket: string,
     tokenListURI: string
   ): Promise<ITokenListProvider & ITokenProvider> {
-    const s3 = new S3()
+    const s3 = new S3({ correctClockSkew: true, maxRetries: 3 })
 
     const cachedTokenList = TOKEN_LIST_CACHE.get<TokenList>(tokenListURI)
 
