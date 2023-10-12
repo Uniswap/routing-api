91fed2ca1d495ace4c8c5fa39f4f37b62cbd9cad
unicorn-here <139156590+unicorn-here@users.noreply.github.com>
2023-10-11 18:56:09 -0700

    fix: routing-api still honor portion amount from URA for exact out (#390)
    * fix: routing-api still need to take in portionAmount from URA for exact out
    
    * prettier
    
    * comments

--- lib/handlers/quote/quote.ts
+++ lib/handlers/quote/quote.ts
@@ -29,9 +29,7 @@ import {
   QUOTE_SPEED_CONFIG,
   INTENT_SPECIFIC_CONFIG,
   FEE_ON_TRANSFER_SPECIFIC_CONFIG,
-  parseFeeOptions,
-  parseFlatFeeOptions,
-  computePortionAmount,
+  populateFeeOptions,
 } from '../shared'
 import { QuoteQueryParams, QuoteQueryParamsJoi } from './schema/quote-schema'
 import { utils } from 'ethers'
@@ -140,6 +138,7 @@ export class QuoteHandler extends APIGLambdaHandler<
         intent,
         enableFeeOnTransferFeeFetching,
         portionBips,
+        portionAmount,
         portionRecipient,
       },
       requestInjected: {
@@ -264,19 +263,14 @@ export class QuoteHandler extends APIGLambdaHandler<
 
       // TODO: Remove once universal router is no longer behind a feature flag.
       if (enableUniversalRouter) {
+        const allFeeOptions = populateFeeOptions(type, portionBips, portionRecipient, portionAmount)
+
         swapParams = {
           type: SwapType.UNIVERSAL_ROUTER,
           deadlineOrPreviousBlockhash: parseDeadline(deadline),
           recipient: recipient,
           slippageTolerance: slippageTolerancePercent,
-          fee: type === 'exactIn' ? parseFeeOptions(portionBips, portionRecipient) : undefined,
-          flatFee:
-            type === 'exactOut'
-              ? parseFlatFeeOptions(
-                  computePortionAmount(CurrencyAmount.fromRawAmount(currencyIn, JSBI.BigInt(amountRaw)), portionBips),
-                  portionRecipient
-                )
-              : undefined,
+          ...allFeeOptions,
         }
       } else {
         swapParams = {
@@ -438,7 +432,7 @@ export class QuoteHandler extends APIGLambdaHandler<
       blockNumber,
       simulationStatus,
       hitsCachedRoute,
-      portionAmount,
+      portionAmount: outputPortionAmount, // TODO: name it back to portionAmount
     } = swapRoute
 
     if (simulationStatus == SimulationStatus.Failed) {
@@ -598,8 +592,8 @@ export class QuoteHandler extends APIGLambdaHandler<
       hitsCachedRoutes: hitsCachedRoute,
       portionBips: portionBips,
       portionRecipient: portionRecipient,
-      portionAmount: portionAmount?.quotient.toString(),
-      portionAmountDecimals: portionAmount?.toExact(),
+      portionAmount: outputPortionAmount?.quotient.toString(),
+      portionAmountDecimals: outputPortionAmount?.toExact(),
     }
 
     this.logRouteMetrics(
--- test/mocha/integ/quote.test.ts
+++ test/mocha/integ/quote.test.ts
@@ -1675,6 +1675,9 @@ describe('quote', function () {
                   enableUniversalRouter: true,
                   simulateFromAddress: alice.address,
                   portionBips: FLAT_PORTION.bips,
+                  portionAmount: CurrencyAmount.fromRawAmount(tokenOut, amount)
+                    .multiply(new Fraction(FLAT_PORTION.bips, 10_000))
+                    .quotient.toString(),
                   portionRecipient: FLAT_PORTION.recipient,
                 }
 
