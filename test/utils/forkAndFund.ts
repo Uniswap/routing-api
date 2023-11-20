3bfb618be9df5e04d913e53d35106bdd64be6fc3
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2023-11-20 12:32:20 -0800

    Test-Fix: fix bullet whale address insufficient gas (#429)
    * fix bullet whale address insufficient gas
    
    * fix prettier

--- test/utils/forkAndFund.ts
+++ test/utils/forkAndFund.ts
@@ -62,6 +62,12 @@ export const resetAndFundAtBlock = async (
       const whale = WHALES[i]
       const whaleAccount = ethers.provider.getSigner(whale)
       try {
+        // Send native ETH from hardhat alice test address, so that whale accounts have sufficient ETH to pay for gas
+        await alice.sendTransaction({
+          to: whale,
+          value: ethers.utils.parseEther('0.1'), // Sends exactly 0.1 ether
+        })
+
         const whaleToken: Erc20 = Erc20__factory.connect(currency.wrapped.address, whaleAccount)
 
         await whaleToken.transfer(alice.address, ethers.utils.parseUnits(amount, currency.decimals))
@@ -69,7 +75,9 @@ export const resetAndFundAtBlock = async (
         break
       } catch (err) {
         if (i == WHALES.length - 1) {
-          throw new Error(`Could not fund ${amount} ${currency.symbol} from any whales.`)
+          throw new Error(
+            `Could not fund ${amount} ${currency.symbol} from any whales. Original error ${JSON.stringify(err)}`
+          )
         }
       }
     }
