bce447a6409b5c2aaae2232600ee670e427269ac
Andy Smith <andy.smith@uniswap.org>
2023-12-16 16:20:01 -0700

    Extract methods in CurrencyLookup.

--- lib/handlers/CurrencyLookup.ts
+++ lib/handlers/CurrencyLookup.ts
@@ -15,18 +15,36 @@ export class CurrencyLookup {
   ) {}
 
   public async searchForToken(tokenRaw: string, chainId: number): Promise<Currency | undefined> {
-    if (NATIVE_NAMES_BY_ID[chainId]!.includes(tokenRaw)) {
-      const nativeToken = nativeOnChain(chainId)
-      this.log.debug(
-        {
-          tokenAddress: nativeToken.wrapped.address,
-        },
-        `Found native token ${tokenRaw} for chain ${chainId}: ${nativeToken.wrapped.address}}`
-      )
+    const nativeToken = this.checkIfNativeToken(tokenRaw, chainId)
+    if (nativeToken) {
       return nativeToken
     }
 
     // At this point, we know this is not a NativeCurrency based on the check above, so we can explicitly cast to Token.
+    const tokenFromTokenList: Token | undefined = await this.checkTokenLists(tokenRaw)
+    if (tokenFromTokenList) {
+      return tokenFromTokenList
+    }
+
+    return await this.checkOnChain(tokenRaw)
+  }
+
+  checkIfNativeToken = (tokenRaw: string, chainId: number): Currency | undefined => {
+    if (!NATIVE_NAMES_BY_ID[chainId] || !NATIVE_NAMES_BY_ID[chainId].includes(tokenRaw)) {
+      return undefined
+    }
+
+    const nativeToken = nativeOnChain(chainId)
+    this.log.debug(
+      {
+        tokenAddress: nativeToken.wrapped.address,
+      },
+      `Found native token ${tokenRaw} for chain ${chainId}: ${nativeToken.wrapped.address}}`
+    )
+    return nativeToken
+  }
+
+  checkTokenLists = async (tokenRaw: string): Promise<Token | undefined> => {
     let token: Token | undefined = undefined
     if (isAddress(tokenRaw)) {
       token = await this.tokenListProvider.getTokenByAddress(tokenRaw)
@@ -43,15 +61,22 @@ export class CurrencyLookup {
         },
         `Found token ${tokenRaw} in token lists.`
       )
-      return token
     }
 
+    return token
+  }
+
+  checkOnChain = async (tokenRaw: string): Promise<Token | undefined> => {
     this.log.debug(`Getting input token ${tokenRaw} from chain`)
-    if (!token && isAddress(tokenRaw)) {
-      const tokenAccessor = await this.tokenProvider.getTokens([tokenRaw])
-      return tokenAccessor.getTokenByAddress(tokenRaw)
+
+    // The ITokenListProvider interface expects a list of addresses to lookup tokens.
+    // If this isn't an address, we can't do the lookup.
+    // https://github.com/Uniswap/smart-order-router/blob/71fac1905a32af369e30e9cbb52ea36e971ab279/src/providers/token-provider.ts#L23
+    if (!isAddress(tokenRaw)) {
+      return undefined
     }
 
-    return undefined
+    const tokenAccessor = await this.tokenProvider.getTokens([tokenRaw])
+    return tokenAccessor.getTokenByAddress(tokenRaw)
   }
 }
