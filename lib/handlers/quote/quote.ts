731f49b47067a39836eb9916e6446f8283553575
Andy Smith <andy.smith@uniswap.org>
2023-12-20 09:59:32 -0700

    Extract determineSwapParams

--- lib/handlers/quote/quote.ts
+++ lib/handlers/quote/quote.ts
@@ -253,92 +253,27 @@ export class QuoteHandler extends APIGLambdaHandler<
 
     metric.putMetric(`${intent}Intent`, 1, MetricLoggerUnit.Count)
 
-    let swapParams: SwapOptions | undefined = undefined
-
-    // e.g. Inputs of form "1.25%" with 2dp max. Convert to fractional representation => 1.25 => 125 / 10000
-    if (slippageTolerance) {
-      const slippageTolerancePercent = parseSlippageTolerance(slippageTolerance)
-
-      // TODO: Remove once universal router is no longer behind a feature flag.
-      if (enableUniversalRouter) {
-        const allFeeOptions = populateFeeOptions(
-          type,
-          portionBips,
-          portionRecipient,
-          portionAmount ??
-            computePortionAmount(CurrencyAmount.fromRawAmount(currencyOut, JSBI.BigInt(amountRaw)), portionBips)
-        )
-
-        swapParams = {
-          type: SwapType.UNIVERSAL_ROUTER,
-          deadlineOrPreviousBlockhash: deadline ? parseDeadline(deadline) : undefined,
-          recipient: recipient,
-          slippageTolerance: slippageTolerancePercent,
-          ...allFeeOptions,
-        }
-      } else {
-        if (deadline && recipient) {
-          swapParams = {
-            type: SwapType.SWAP_ROUTER_02,
-            deadline: parseDeadline(deadline),
-            recipient: recipient,
-            slippageTolerance: slippageTolerancePercent,
-          }
-        }
-      }
-
-      if (
-        enableUniversalRouter &&
-        permitSignature &&
-        permitNonce &&
-        permitExpiration &&
-        permitAmount &&
-        permitSigDeadline
-      ) {
-        const permit: PermitSingle = {
-          details: {
-            token: currencyIn.wrapped.address,
-            amount: permitAmount,
-            expiration: permitExpiration,
-            nonce: permitNonce,
-          },
-          spender: UNIVERSAL_ROUTER_ADDRESS(chainId),
-          sigDeadline: permitSigDeadline,
-        }
-
-        if (swapParams) {
-          swapParams.inputTokenPermit = {
-            ...permit,
-            signature: permitSignature,
-          }
-        }
-      } else if (
-        !enableUniversalRouter &&
-        permitSignature &&
-        ((permitNonce && permitExpiration) || (permitAmount && permitSigDeadline))
-      ) {
-        const { v, r, s } = utils.splitSignature(permitSignature)
-
-        if (swapParams) {
-          swapParams.inputTokenPermit = {
-            v: v as 0 | 1 | 27 | 28,
-            r,
-            s,
-            ...(permitNonce && permitExpiration
-              ? { nonce: permitNonce!, expiry: permitExpiration! }
-              : { amount: permitAmount!, deadline: permitSigDeadline! }),
-          }
-        }
-      }
-
-      if (simulateFromAddress) {
-        metric.putMetric('Simulation Requested', 1, MetricLoggerUnit.Count)
-
-        if (swapParams) {
-          swapParams.simulate = { fromAddress: simulateFromAddress }
-        }
-      }
-    }
+    const swapParams: SwapOptions | undefined = this.determineSwapParams(
+      slippageTolerance,
+      enableUniversalRouter,
+      type,
+      portionBips,
+      portionRecipient,
+      portionAmount,
+      currencyOut,
+      amountRaw,
+      deadline,
+      recipient,
+      permitSignature,
+      permitNonce,
+      permitExpiration,
+      permitAmount,
+      permitSigDeadline,
+      currencyIn,
+      chainId,
+      simulateFromAddress,
+      metric
+    )
 
     let swapRoute: SwapRoute | null
     let amount: CurrencyAmount<Currency>
@@ -626,6 +561,116 @@ export class QuoteHandler extends APIGLambdaHandler<
       statusCode: 200,
       body: result,
     }
+  }
+
+  private determineSwapParams(
+    slippageTolerance: string | undefined,
+    enableUniversalRouter: boolean | undefined,
+    type: string,
+    portionBips: number | undefined,
+    portionRecipient: string | undefined,
+    portionAmount: string | undefined,
+    currencyOut: Currency,
+    amountRaw: string,
+    deadline: string | undefined,
+    recipient: string | undefined,
+    permitSignature: string | undefined,
+    permitNonce: string | undefined,
+    permitExpiration: string | undefined,
+    permitAmount: string | undefined,
+    permitSigDeadline: string | undefined,
+    currencyIn: Currency,
+    chainId: ChainId,
+    simulateFromAddress: string | undefined,
+    metric: IMetric
+  ) {
+    let swapParams: SwapOptions | undefined = undefined
+
+    // e.g. Inputs of form "1.25%" with 2dp max. Convert to fractional representation => 1.25 => 125 / 10000
+    if (slippageTolerance) {
+      const slippageTolerancePercent = parseSlippageTolerance(slippageTolerance)
+
+      // TODO: Remove once universal router is no longer behind a feature flag.
+      if (enableUniversalRouter) {
+        const allFeeOptions = populateFeeOptions(
+          type,
+          portionBips,
+          portionRecipient,
+          portionAmount ??
+            computePortionAmount(CurrencyAmount.fromRawAmount(currencyOut, JSBI.BigInt(amountRaw)), portionBips)
+        )
+
+        swapParams = {
+          type: SwapType.UNIVERSAL_ROUTER,
+          deadlineOrPreviousBlockhash: deadline ? parseDeadline(deadline) : undefined,
+          recipient: recipient,
+          slippageTolerance: slippageTolerancePercent,
+          ...allFeeOptions,
+        }
+      } else {
+        if (deadline && recipient) {
+          swapParams = {
+            type: SwapType.SWAP_ROUTER_02,
+            deadline: parseDeadline(deadline),
+            recipient: recipient,
+            slippageTolerance: slippageTolerancePercent,
+          }
+        }
+      }
+
+      if (
+        enableUniversalRouter &&
+        permitSignature &&
+        permitNonce &&
+        permitExpiration &&
+        permitAmount &&
+        permitSigDeadline
+      ) {
+        const permit: PermitSingle = {
+          details: {
+            token: currencyIn.wrapped.address,
+            amount: permitAmount,
+            expiration: permitExpiration,
+            nonce: permitNonce,
+          },
+          spender: UNIVERSAL_ROUTER_ADDRESS(chainId),
+          sigDeadline: permitSigDeadline,
+        }
+
+        if (swapParams) {
+          swapParams.inputTokenPermit = {
+            ...permit,
+            signature: permitSignature,
+          }
+        }
+      } else if (
+        !enableUniversalRouter &&
+        permitSignature &&
+        ((permitNonce && permitExpiration) || (permitAmount && permitSigDeadline))
+      ) {
+        const { v, r, s } = utils.splitSignature(permitSignature)
+
+        if (swapParams) {
+          swapParams.inputTokenPermit = {
+            v: v as 0 | 1 | 27 | 28,
+            r,
+            s,
+            ...(permitNonce && permitExpiration
+              ? { nonce: permitNonce!, expiry: permitExpiration! }
+              : { amount: permitAmount!, deadline: permitSigDeadline! }),
+          }
+        }
+      }
+
+      if (simulateFromAddress) {
+        metric.putMetric('Simulation Requested', 1, MetricLoggerUnit.Count)
+
+        if (swapParams) {
+          swapParams.simulate = { fromAddress: simulateFromAddress }
+        }
+      }
+    }
+    return swapParams
   }
 
   private deriveBuyFeeBps(
