3c61a474b4c019b5b2dc26f9c5cbb3d8238ef32c
Andy Smith <andy.smith@uniswap.org>
2023-12-20 12:11:27 -0700

    Invert conditional to exit early if slippageTolerance is undefined.

--- lib/handlers/quote/SwapOptionsFactory.ts
+++ lib/handlers/quote/SwapOptionsFactory.ts
@@ -29,90 +29,94 @@ export class SwapOptionsFactory {
     simulateFromAddress: string | undefined,
     metric: IMetric
   ): SwapOptions | undefined {
+    // slippageTolerance looks like it's required for both the UniversalRouter and SwapRouter02.
+    // If it's undefined, we'll exit early and not request any call data generation from SOR.
+    if (!slippageTolerance) {
+      return undefined
+    }
+
     let swapParams: SwapOptions | undefined = undefined
 
     // e.g. Inputs of form "1.25%" with 2dp max. Convert to fractional representation => 1.25 => 125 / 10000
-    if (slippageTolerance) {
-      const slippageTolerancePercent = parseSlippageTolerance(slippageTolerance)
+    const slippageTolerancePercent = parseSlippageTolerance(slippageTolerance)
 
-      // TODO: Remove once universal router is no longer behind a feature flag.
-      if (enableUniversalRouter) {
-        const allFeeOptions = populateFeeOptions(
-          tradeType,
-          portionBips,
-          portionRecipient,
-          portionAmount ??
-            computePortionAmount(CurrencyAmount.fromRawAmount(currencyOut, JSBI.BigInt(amountRaw)), portionBips)
-        )
+    // TODO: Remove once universal router is no longer behind a feature flag.
+    if (enableUniversalRouter) {
+      const allFeeOptions = populateFeeOptions(
+        tradeType,
+        portionBips,
+        portionRecipient,
+        portionAmount ??
+          computePortionAmount(CurrencyAmount.fromRawAmount(currencyOut, JSBI.BigInt(amountRaw)), portionBips)
+      )
 
+      swapParams = {
+        type: SwapType.UNIVERSAL_ROUTER,
+        deadlineOrPreviousBlockhash: deadline ? parseDeadline(deadline) : undefined,
+        recipient: recipient,
+        slippageTolerance: slippageTolerancePercent,
+        ...allFeeOptions,
+      }
+    } else {
+      if (deadline && recipient) {
         swapParams = {
-          type: SwapType.UNIVERSAL_ROUTER,
-          deadlineOrPreviousBlockhash: deadline ? parseDeadline(deadline) : undefined,
+          type: SwapType.SWAP_ROUTER_02,
+          deadline: parseDeadline(deadline),
           recipient: recipient,
           slippageTolerance: slippageTolerancePercent,
-          ...allFeeOptions,
-        }
-      } else {
-        if (deadline && recipient) {
-          swapParams = {
-            type: SwapType.SWAP_ROUTER_02,
-            deadline: parseDeadline(deadline),
-            recipient: recipient,
-            slippageTolerance: slippageTolerancePercent,
-          }
         }
       }
+    }
 
-      if (
-        enableUniversalRouter &&
-        permitSignature &&
-        permitNonce &&
-        permitExpiration &&
-        permitAmount &&
-        permitSigDeadline
-      ) {
-        const permit: PermitSingle = {
-          details: {
-            token: currencyIn.wrapped.address,
-            amount: permitAmount,
-            expiration: permitExpiration,
-            nonce: permitNonce,
-          },
-          spender: UNIVERSAL_ROUTER_ADDRESS(chainId),
-          sigDeadline: permitSigDeadline,
-        }
-
-        if (swapParams) {
-          swapParams.inputTokenPermit = {
-            ...permit,
-            signature: permitSignature,
-          }
-        }
-      } else if (
-        !enableUniversalRouter &&
-        permitSignature &&
-        ((permitNonce && permitExpiration) || (permitAmount && permitSigDeadline))
-      ) {
-        const { v, r, s } = utils.splitSignature(permitSignature)
-
-        if (swapParams) {
-          swapParams.inputTokenPermit = {
-            v: v as 0 | 1 | 27 | 28,
-            r,
-            s,
-            ...(permitNonce && permitExpiration
-              ? { nonce: permitNonce!, expiry: permitExpiration! }
-              : { amount: permitAmount!, deadline: permitSigDeadline! }),
-          }
-        }
+    if (
+      enableUniversalRouter &&
+      permitSignature &&
+      permitNonce &&
+      permitExpiration &&
+      permitAmount &&
+      permitSigDeadline
+    ) {
+      const permit: PermitSingle = {
+        details: {
+          token: currencyIn.wrapped.address,
+          amount: permitAmount,
+          expiration: permitExpiration,
+          nonce: permitNonce,
+        },
+        spender: UNIVERSAL_ROUTER_ADDRESS(chainId),
+        sigDeadline: permitSigDeadline,
       }
 
-      if (simulateFromAddress) {
-        metric.putMetric('Simulation Requested', 1, MetricLoggerUnit.Count)
-
-        if (swapParams) {
-          swapParams.simulate = { fromAddress: simulateFromAddress }
+      if (swapParams) {
+        swapParams.inputTokenPermit = {
+          ...permit,
+          signature: permitSignature,
         }
+      }
+    } else if (
+      !enableUniversalRouter &&
+      permitSignature &&
+      ((permitNonce && permitExpiration) || (permitAmount && permitSigDeadline))
+    ) {
+      const { v, r, s } = utils.splitSignature(permitSignature)
+
+      if (swapParams) {
+        swapParams.inputTokenPermit = {
+          v: v as 0 | 1 | 27 | 28,
+          r,
+          s,
+          ...(permitNonce && permitExpiration
+            ? { nonce: permitNonce!, expiry: permitExpiration! }
+            : { amount: permitAmount!, deadline: permitSigDeadline! }),
+        }
+      }
+    }
+
+    if (simulateFromAddress) {
+      metric.putMetric('Simulation Requested', 1, MetricLoggerUnit.Count)
+
+      if (swapParams) {
+        swapParams.simulate = { fromAddress: simulateFromAddress }
       }
     }
     return swapParams
