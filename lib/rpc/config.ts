c4d8016bd601f499f41caa61b9646926c666e6cf
Jie Hou <jie.hou@uniswap.org>
2024-01-19 11:49:45 -0800

    Change config value for 1st launch

--- lib/rpc/config.ts
+++ lib/rpc/config.ts
@@ -13,10 +13,10 @@ export const DEFAULT_CONFIG: Config = {
   ERROR_PENALTY: -50,
   HIGH_LATENCY_PENALTY: -20,
   // If a healthy provider's score drop below this, it will become unhealthy.
-  HEALTH_SCORE_FALLBACK_THRESHOLD: -3990,
+  HEALTH_SCORE_FALLBACK_THRESHOLD: -999990,
   // If an unhealthy provider's score raise above this, it will become healthy.
-  HEALTH_SCORE_RECOVER_THRESHOLD: -20,
-  MAX_LATENCY_ALLOWED_IN_MS: 500,
+  HEALTH_SCORE_RECOVER_THRESHOLD: -200,
+  MAX_LATENCY_ALLOWED_IN_MS: 4000,
   // As time passes, provider's health score will automatically increase,
   // but will not exceed 0. This defines the score increased every millisecond.
   RECOVER_SCORE_PER_MS: 0.01,
