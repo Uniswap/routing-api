351aa17be4fb45a7cd563634a27da334c2ada725
Jie Hou <jie.hou@uniswap.org>
2024-02-19 06:15:41 -0800

    Use io-ts to redefine ProdConfig

--- lib/rpc/ProdConfig.ts
+++ lib/rpc/ProdConfig.ts
@@ -1,10 +1,25 @@
-import { ChainId } from '@uniswap/sdk-core'
+import * as t from "io-ts"
+// import { ChainId } from '@uniswap/sdk-core'
 
-export interface ChainConfig {
-  useMultiProvider: boolean
-  sessionAllowProviderFallbackWhenUnhealthy?: boolean
-  providerInitialWeights?: number[]
-  providerUrls?: string[]
-}
+export const ProdConfigCodec = t.array(t.intersection([
+  t.type({
+    chainId: t.number,
+    useMultiProvider: t.boolean,
+  }),
+  t.partial({
+    sessionAllowProviderFallbackWhenUnhealthy: t.boolean,
+    providerInitialWeights: t.array(t.number),
+    providerUrls: t.array(t.string),
+  })
+]))
 
-export type ProdConfig = Map<ChainId, ChainConfig>
+export type ProdConfig = t.TypeOf<typeof ProdConfigCodec>
+
+// export interface ChainConfig {
+//   useMultiProvider: boolean
+//   sessionAllowProviderFallbackWhenUnhealthy?: boolean
+//   providerInitialWeights?: number[]
+//   providerUrls?: string[]
+// }
+
+// export type ProdConfig = Map<ChainId, ChainConfig>
