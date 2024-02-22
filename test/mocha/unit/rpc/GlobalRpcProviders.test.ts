0c3d389fde7e9e3df42f1064b6471a53569429ee
Jie Hou <jie.hou@uniswap.org>
2024-02-22 12:26:08 -0800

    Revert "Use separate envvar / AWS secret to contain all provider config (#468)"
    This reverts commit 0cece8c860309fb1d2855730a5d28f113a34a6a3.

--- test/mocha/unit/rpc/GlobalRpcProviders.test.ts
+++ test/mocha/unit/rpc/GlobalRpcProviders.test.ts
@@ -10,7 +10,6 @@ const UNI_PROVIDER_TEST_CONFIG: UniJsonRpcProviderConfig = {
   RECOVER_EVALUATION_WAIT_PERIOD_IN_MS: 5000,
   ENABLE_SHADOW_LATENCY_EVALUATION: false,
   LATENCY_EVALUATION_WAIT_PERIOD_IN_S: 15,
-  DEFAULT_INITIAL_WEIGHT: 1000,
 }
 
 const SINGLE_PROVIDER_TEST_CONFIG: SingleJsonRpcProviderConfig = {
@@ -30,8 +29,9 @@ const SINGLE_PROVIDER_TEST_CONFIG: SingleJsonRpcProviderConfig = {
 describe('GlobalRpcProviders', () => {
   it('Prepare global UniJsonRpcProvider by reading config', () => {
     process.env = {
-      UNI_RPC_PROVIDER_PROD_CONFIG:
-        '[{"chainId":1,"useMultiProvider":false},{"chainId":43114,"useMultiProvider":true,"sessionAllowProviderFallbackWhenUnhealthy":true,"providerInitialWeights":[2,1],"providerUrls":["url0","url1"]}]',
+      UNI_RPC_PROVIDER_CONFIG: '{"1": "false", "43114": "true"}',
+      WEB3_RPC_1: 'url_1',
+      WEB3_RPC_43114: 'url_43114',
     }
 
     expect(
@@ -42,24 +42,13 @@ describe('GlobalRpcProviders', () => {
 
     expect(
       GlobalRpcProviders.getGlobalUniRpcProviders(log, UNI_PROVIDER_TEST_CONFIG, SINGLE_PROVIDER_TEST_CONFIG).has(
-        ChainId.BNB
-      )
-    ).to.be.false
-
-    expect(
-      GlobalRpcProviders.getGlobalUniRpcProviders(log, UNI_PROVIDER_TEST_CONFIG, SINGLE_PROVIDER_TEST_CONFIG).has(
         ChainId.AVALANCHE
       )
     ).to.be.true
-
-    const avaUniProvider = GlobalRpcProviders.getGlobalUniRpcProviders(
-      log,
-      UNI_PROVIDER_TEST_CONFIG,
-      SINGLE_PROVIDER_TEST_CONFIG
-    ).get(ChainId.AVALANCHE)!
-    expect(avaUniProvider['sessionAllowProviderFallbackWhenUnhealthy']).to.be.true
-    expect(avaUniProvider['urlWeight']).to.deep.equal({ url0: 2, url1: 1 })
-    expect(avaUniProvider['providers'][0].url).to.equal('url0')
-    expect(avaUniProvider['providers'][1].url).to.equal('url1')
+    expect(
+      GlobalRpcProviders.getGlobalUniRpcProviders(log, UNI_PROVIDER_TEST_CONFIG, SINGLE_PROVIDER_TEST_CONFIG).get(
+        ChainId.AVALANCHE
+      )?.chainId
+    ).to.be.equal(ChainId.AVALANCHE)
   })
 })
