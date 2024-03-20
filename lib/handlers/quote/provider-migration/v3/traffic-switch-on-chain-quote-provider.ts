aec51f95fcae291f36512d86d24273f8f884ac31
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-03-20 12:56:19 -0700

    on-chain quote provider traffic switch

---
+++ lib/handlers/quote/provider-migration/v3/traffic-switch-on-chain-quote-provider.ts
@@ -0,0 +1,91 @@
+import { IOnChainQuoteProvider, metric, MetricLoggerUnit, RouteWithQuotes } from '@uniswap/smart-order-router'
+import { MixedRoute, V2Route, V3Route } from '@uniswap/smart-order-router/build/main/routers'
+import { Currency, CurrencyAmount } from '@uniswap/sdk-core'
+import { ProviderConfig } from '@uniswap/smart-order-router/build/main/providers/provider'
+import { BigNumber } from 'ethers'
+import { QUOTE_PROVIDER_TRAFFIC_SWITCH_CONFIGURATION } from '../../util/quote-provider-traffic-switch-configuration'
+
+export type TrafficSwitchOnChainQuoteProviderProps = {
+  currentQuoteProvider: IOnChainQuoteProvider
+  targetQuoteProvider: IOnChainQuoteProvider
+}
+
+export class TrafficSwitchOnChainQuoteProvider implements IOnChainQuoteProvider {
+  private readonly currentQuoteProvider: IOnChainQuoteProvider
+  private readonly targetQuoteProvider: IOnChainQuoteProvider
+
+  protected readonly SHOULD_SWITCH_TRAFFIC = () =>
+    QUOTE_PROVIDER_TRAFFIC_SWITCH_CONFIGURATION.switchPercentage > this.getRandomPercentage()
+  protected readonly SHOULD_SAMPLE_TRAFFIC = () =>
+    QUOTE_PROVIDER_TRAFFIC_SWITCH_CONFIGURATION.samplingPercentage > this.getRandomPercentage()
+
+
+  constructor(props: TrafficSwitchOnChainQuoteProviderProps) {
+    this.currentQuoteProvider = props.currentQuoteProvider
+    this.targetQuoteProvider = props.targetQuoteProvider
+  }
+
+  async getQuotesManyExactIn<TRoute extends V3Route | V2Route | MixedRoute>(amountIns: CurrencyAmount<Currency>[], routes: TRoute[], providerConfig?: ProviderConfig): Promise<{
+    routesWithQuotes: RouteWithQuotes<TRoute>[];
+    blockNumber: BigNumber
+  }> {
+    const sampleTraffic = this.SHOULD_SAMPLE_TRAFFIC()
+    const switchTraffic = this.SHOULD_SWITCH_TRAFFIC()
+
+    let currentQuote;
+    let targetQuote;
+
+    metric.putMetric('ON_CHAIN_QUOTE_PROVIDER_EXACT_IN_TRAFFIC_TOTAL', 1, MetricLoggerUnit.None)
+
+    if (sampleTraffic) {
+      metric.putMetric('ON_CHAIN_QUOTE_PROVIDER_EXACT_IN_TRAFFIC_SAMPLING', 1, MetricLoggerUnit.None)
+
+      currentQuote = await this.currentQuoteProvider.getQuotesManyExactIn(amountIns, routes, providerConfig)
+      targetQuote = await this.targetQuoteProvider.getQuotesManyExactIn(amountIns, routes, providerConfig)
+    }
+
+    if (switchTraffic) {
+      metric.putMetric('ON_CHAIN_QUOTE_PROVIDER_EXACT_IN_TRAFFIC_TARGET', 1, MetricLoggerUnit.None)
+
+      return targetQuote ?? await this.targetQuoteProvider.getQuotesManyExactIn(amountIns, routes, providerConfig)
+    } else {
+      metric.putMetric('ON_CHAIN_QUOTE_PROVIDER_EXACT_IN_TRAFFIC_CURRENT', 1, MetricLoggerUnit.None)
+
+      return currentQuote ?? await this.currentQuoteProvider.getQuotesManyExactIn(amountIns, routes, providerConfig)
+    }
+  }
+
+  async getQuotesManyExactOut<TRoute extends V3Route>(amountOuts: CurrencyAmount<Currency>[], routes: TRoute[], providerConfig?: ProviderConfig): Promise<{
+    routesWithQuotes: RouteWithQuotes<TRoute>[];
+    blockNumber: BigNumber
+  }> {
+    const sampleTraffic = this.SHOULD_SAMPLE_TRAFFIC()
+    const switchTraffic = this.SHOULD_SWITCH_TRAFFIC()
+
+    let currentQuote;
+    let targetQuote;
+
+    metric.putMetric('ON_CHAIN_QUOTE_PROVIDER_EXACT_OUT_TRAFFIC_TOTAL', 1, MetricLoggerUnit.None)
+
+    if (sampleTraffic) {
+      metric.putMetric('ON_CHAIN_QUOTE_PROVIDER_EXACT_OUT_TRAFFIC_SAMPLING', 1, MetricLoggerUnit.None)
+
+      currentQuote = await this.currentQuoteProvider.getQuotesManyExactOut(amountOuts, routes, providerConfig)
+      targetQuote = await this.targetQuoteProvider.getQuotesManyExactOut(amountOuts, routes, providerConfig)
+    }
+
+    if (switchTraffic) {
+      metric.putMetric('ON_CHAIN_QUOTE_PROVIDER_EXACT_OUT_TRAFFIC_TARGET', 1, MetricLoggerUnit.None)
+
+      return targetQuote ?? await this.targetQuoteProvider.getQuotesManyExactOut(amountOuts, routes, providerConfig)
+    } else {
+      metric.putMetric('ON_CHAIN_QUOTE_PROVIDER_EXACT_OUT_TRAFFIC_CURRENT', 1, MetricLoggerUnit.None)
+
+      return currentQuote ?? await this.currentQuoteProvider.getQuotesManyExactOut(amountOuts, routes, providerConfig)
+    }
+  }
+
+  private getRandomPercentage(): number {
+    return Math.floor(Math.random() * 100)
+  }
+}
---
+++ lib/handlers/quote/util/quote-provider-traffic-switch-configuration.ts
@@ -0,0 +1,5 @@
+export const QUOTE_PROVIDER_TRAFFIC_SWITCH_CONFIGURATION = {
+  switchPercentage: 0.0,
+  samplingPercentage: 0.1,
+}
+
