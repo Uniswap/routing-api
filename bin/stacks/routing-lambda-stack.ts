684d0c0d5ee301104b1ba1014fbdd8e8b94dce60
Miguel Cervera <miguel.cervera@uniswap.org>
2024-03-20 14:07:48 -0700

    Lambda version bump (#517)
    Co-authored-by: jsy1218 <91580504+jsy1218@users.noreply.github.com>

--- bin/stacks/routing-lambda-stack.ts
+++ bin/stacks/routing-lambda-stack.ts
@@ -112,7 +112,7 @@ export class RoutingLambdaStack extends cdk.NestedStack {
 
       description: 'Routing Lambda',
       environment: {
-        VERSION: '12',
+        VERSION: '14',
         NODE_OPTIONS: '--enable-source-maps',
         POOL_CACHE_BUCKET: poolCacheBucket.bucketName,
         POOL_CACHE_BUCKET_2: poolCacheBucket2.bucketName,
