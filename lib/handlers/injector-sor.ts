c6f0507f98f52dfd574d9e890828b31ef02ce8d8
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-03-22 14:39:01 -0700

    Revert "change to polygon to begin with"
    This reverts commit 7d2c5cc698d5e8c7dee8f40166b0ba4b60a31463.

--- lib/handlers/injector-sor.ts
+++ lib/handlers/injector-sor.ts
@@ -271,7 +271,7 @@ export abstract class InjectorSOR<Router, QueryParams> extends Injector<
           // 200*725k < 150m
           let quoteProvider: IOnChainQuoteProvider | undefined = undefined
           switch (chainId) {
-            case ChainId.POLYGON:
+            case ChainId.MAINNET:
               const currentQuoteProvider = new OnChainQuoteProvider(
                 chainId,
                 provider,
