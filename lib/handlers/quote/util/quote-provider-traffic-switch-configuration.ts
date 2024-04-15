20c5ac974c334d924096262c616f90ece5e156a0
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-04-15 13:03:57 -0700

    feat: shadow sampling view-only quoter on mainnet (#584)
    shadow sampling view-only quoter on mainnet

--- lib/handlers/quote/util/quote-provider-traffic-switch-configuration.ts
+++ lib/handlers/quote/util/quote-provider-traffic-switch-configuration.ts
@@ -20,6 +20,14 @@ export const QUOTE_PROVIDER_TRAFFIC_SWITCH_CONFIGURATION = (
         switchExactOutPercentage: 0.0,
         samplingExactOutPercentage: 100,
       } as QuoteProviderTrafficSwitchConfiguration
+    case ChainId.MAINNET:
+      // Total RPM for 'QuoteTotalCallsToProvider' is around 20k-30k (across all chains), so 0.1% means 20-30 RPM shadow sampling
+      return {
+        switchExactInPercentage: 0.0,
+        samplingExactInPercentage: 0.1,
+        switchExactOutPercentage: 0.0,
+        samplingExactOutPercentage: 0.1,
+      } as QuoteProviderTrafficSwitchConfiguration
     // If we accidentally switch a traffic, we have the protection to shadow sample only 0.1% of traffic
     default:
       return {
