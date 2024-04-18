832014ad00ff7adfc13cf3dc481b7ced68b1a648
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-04-18 10:16:23 -0700

    shadow sampling view-only quoter on arbitrum

--- lib/handlers/quote/util/quote-provider-traffic-switch-configuration.ts
+++ lib/handlers/quote/util/quote-provider-traffic-switch-configuration.ts
@@ -43,6 +43,14 @@ export const QUOTE_PROVIDER_TRAFFIC_SWITCH_CONFIGURATION = (
         switchExactOutPercentage: 0.0,
         samplingExactOutPercentage: 0.1,
       } as QuoteProviderTrafficSwitchConfiguration
+    case ChainId.ARBITRUM_ONE:
+      // Arbitrum One has around 1k-2k RPM, so 0.5% means 5-10 RPM shadow sampling
+      return {
+        switchExactInPercentage: 0.0,
+        samplingExactInPercentage: 0.5,
+        switchExactOutPercentage: 0.0,
+        samplingExactOutPercentage: 0.5,
+      } as QuoteProviderTrafficSwitchConfiguration
     case ChainId.POLYGON:
       // Total RPM for 'QuoteTotalCallsToProvider' is around 20k-30k (across all chains), so 0.1% means 20-30 RPM shadow sampling
       return {
@@ -51,7 +59,6 @@ export const QUOTE_PROVIDER_TRAFFIC_SWITCH_CONFIGURATION = (
         switchExactOutPercentage: 0.0,
         samplingExactOutPercentage: 0.5,
       } as QuoteProviderTrafficSwitchConfiguration
-
     // If we accidentally switch a traffic, we have the protection to shadow sample only 0.1% of traffic
     default:
       return {
