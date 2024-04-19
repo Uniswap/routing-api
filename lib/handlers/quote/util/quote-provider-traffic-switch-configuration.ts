c358d2f45c5dfc2b1cc197a4cec7bd9f47e9ec14
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-04-19 13:06:03 -0700

    feat: shadow sampling view-only quoter on blast (#611)
    shadow sampling view-only quoter on blast

--- lib/handlers/quote/util/quote-provider-traffic-switch-configuration.ts
+++ lib/handlers/quote/util/quote-provider-traffic-switch-configuration.ts
@@ -67,6 +67,14 @@ export const QUOTE_PROVIDER_TRAFFIC_SWITCH_CONFIGURATION = (
         switchExactOutPercentage: 0.0,
         samplingExactOutPercentage: 1,
       } as QuoteProviderTrafficSwitchConfiguration
+    case ChainId.BLAST:
+      // Blast RPC eth_call traffic is about 1/10 of mainnet, so we can shadow sample 1% of traffic
+      return {
+        switchExactInPercentage: 0.0,
+        samplingExactInPercentage: 1,
+        switchExactOutPercentage: 0.0,
+        samplingExactOutPercentage: 1,
+      } as QuoteProviderTrafficSwitchConfiguration
     case ChainId.BNB:
       // BNB RPC eth_call traffic is about 1/10 of mainnet, so we can shadow sample 1% of traffic
       return {
