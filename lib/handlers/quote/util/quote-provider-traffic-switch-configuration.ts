48275ebac8cf549b62a0aadc0745f6555ec84f9f
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-04-24 15:01:28 -0700

    live switch base quote for 1 percent

--- lib/handlers/quote/util/quote-provider-traffic-switch-configuration.ts
+++ lib/handlers/quote/util/quote-provider-traffic-switch-configuration.ts
@@ -30,10 +30,10 @@ export const QUOTE_PROVIDER_TRAFFIC_SWITCH_CONFIGURATION = (
     case ChainId.BASE:
       // Base RPC eth_call traffic is about double mainnet, so we can shadow sample 0.05% of traffic
       return {
-        switchExactInPercentage: 0.0,
-        samplingExactInPercentage: 0.05,
-        switchExactOutPercentage: 0.0,
-        samplingExactOutPercentage: 0.05,
+        switchExactInPercentage: 1,
+        samplingExactInPercentage: 0,
+        switchExactOutPercentage: 1,
+        samplingExactOutPercentage: 0,
       } as QuoteProviderTrafficSwitchConfiguration
     case ChainId.MAINNET:
       // Total RPM for 'QuoteTotalCallsToProvider' is around 20k-30k (across all chains), so 0.1% means 20-30 RPM shadow sampling
