97c02eb7373a4a329d43459209e3a0c1cd764914
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-04-26 14:48:02 -0700

    shadow sample mixed quoter on Base at 1%

--- lib/handlers/quote/util/quote-provider-traffic-switch-configuration.ts
+++ lib/handlers/quote/util/quote-provider-traffic-switch-configuration.ts
@@ -31,9 +31,9 @@ export const QUOTE_PROVIDER_TRAFFIC_SWITCH_CONFIGURATION = (
       // Base RPC eth_call traffic is about double mainnet, so we can shadow sample 0.05% of traffic
       return {
         switchExactInPercentage: 100,
-        samplingExactInPercentage: 0,
+        samplingExactInPercentage: 1,
         switchExactOutPercentage: 100,
-        samplingExactOutPercentage: 0,
+        samplingExactOutPercentage: 1,
       } as QuoteProviderTrafficSwitchConfiguration
     case ChainId.MAINNET:
       // Total RPM for 'QuoteTotalCallsToProvider' is around 20k-30k (across all chains), so 0.1% means 20-30 RPM shadow sampling
