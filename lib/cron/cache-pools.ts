7cb896432429a2a4002c858cc82a504836af0af3
Miguel Cervera <miguel.cervera@uniswap.org>
2024-04-26 12:15:42 -0700

    prettier

--- lib/cron/cache-pools.ts
+++ lib/cron/cache-pools.ts
@@ -9,70 +9,69 @@ import { chainProtocols } from './cache-config'
 import { AWSMetricsLogger } from '../handlers/router-entities/aws-metrics-logger'
 import { metricScope } from 'aws-embedded-metrics'
 
-const handler: ScheduledHandler = metricScope(metrics =>
-  async (event: EventBridgeEvent<string, void>) => {
-    const beforeAll = Date.now()
-    metrics.setNamespace('Uniswap')
-    metrics.setDimensions({ Service: 'CachePools' })
-    const metric = new AWSMetricsLogger(metrics)
-    setGlobalMetric(metric)
+const handler: ScheduledHandler = metricScope((metrics) => async (event: EventBridgeEvent<string, void>) => {
+  const beforeAll = Date.now()
+  metrics.setNamespace('Uniswap')
+  metrics.setDimensions({ Service: 'CachePools' })
+  const metric = new AWSMetricsLogger(metrics)
+  setGlobalMetric(metric)
 
-    const chainId: ChainId = parseInt(process.env.chainId!)
-    const protocol = process.env.protocol! as Protocol
-    // Don't retry for V2 as it will timeout and throw 500
-    const provider = chainProtocols.find(
-      (element) => element.protocol == protocol && element.chainId == chainId
-    )!.provider
-    const log: Logger = bunyan.createLogger({
-      name: 'RoutingLambda',
-      serializers: bunyan.stdSerializers,
-      level: 'info',
-      requestId: event.id,
+  const chainId: ChainId = parseInt(process.env.chainId!)
+  const protocol = process.env.protocol! as Protocol
+  // Don't retry for V2 as it will timeout and throw 500
+  const provider = chainProtocols.find(
+    (element) => element.protocol == protocol && element.chainId == chainId
+  )!.provider
+  const log: Logger = bunyan.createLogger({
+    name: 'RoutingLambda',
+    serializers: bunyan.stdSerializers,
+    level: 'info',
+    requestId: event.id,
+  })
+  setGlobalLogger(log)
+
+  const s3 = new S3()
+
+  log.info(`Getting pools for ${protocol} on ${chainId}`)
+  const metricPrefix = `CachePools.chain_${chainId}.${protocol}_protocol`
+  metric.putMetric(metricPrefix, 1)
+
+  let pools
+  try {
+    const beforeGetPool = Date.now()
+    pools = await provider.getPools()
+    metric.putMetric(`${metricPrefix}.getPools.latency`, Date.now() - beforeGetPool)
+  } catch (err) {
+    metric.putMetric(`${metricPrefix}.getPools.error`, 1)
+    log.error({ err }, `Failed to get pools for ${protocol} on ${chainId}`)
+    throw new Error(`Failed to get pools for ${protocol} on ${chainId}`)
+  }
+
+  if (!pools || pools.length == 0) {
+    metric.putMetric(`${metricPrefix}.getPools.empty`, 1)
+    log.info(`No ${protocol} pools found from the subgraph for ${chainId.toString()}`)
+    return
+  }
+
+  const beforeS3 = Date.now()
+  const key = S3_POOL_CACHE_KEY(process.env.POOL_CACHE_KEY!, chainId, protocol)
+
+  log.info(`Got ${pools.length} ${protocol} pools from the subgraph for ${chainId.toString()}. Saving to ${key}`)
+
+  const result = await s3
+    .putObject({
+      Bucket: process.env.POOL_CACHE_BUCKET_2!,
+      Key: key,
+      Body: JSON.stringify(pools),
     })
-    setGlobalLogger(log)
+    .promise()
 
-    const s3 = new S3()
+  metric.putMetric(`${metricPrefix}.s3.latency`, Date.now() - beforeS3)
 
-    log.info(`Getting pools for ${protocol} on ${chainId}`)
-    const metricPrefix = `CachePools.chain_${chainId}.${protocol}_protocol`;
-    metric.putMetric(metricPrefix, 1)
+  log.info({ result }, `Done ${protocol} for ${chainId.toString()}`)
 
-    let pools
-    try {
-      const beforeGetPool = Date.now()
-      pools = await provider.getPools()
-      metric.putMetric(`${metricPrefix}.getPools.latency`, Date.now() - beforeGetPool)
-    } catch (err) {
-      metric.putMetric(`${metricPrefix}.getPools.error`, 1)
-      log.error({ err }, `Failed to get pools for ${protocol} on ${chainId}`)
-      throw new Error(`Failed to get pools for ${protocol} on ${chainId}`)
-    }
-
-    if (!pools || pools.length == 0) {
-      metric.putMetric(`${metricPrefix}.getPools.empty`, 1)
-      log.info(`No ${protocol} pools found from the subgraph for ${chainId.toString()}`)
-      return
-    }
-
-    const beforeS3 = Date.now();
-    const key = S3_POOL_CACHE_KEY(process.env.POOL_CACHE_KEY!, chainId, protocol)
-
-    log.info(`Got ${pools.length} ${protocol} pools from the subgraph for ${chainId.toString()}. Saving to ${key}`)
-
-    const result = await s3
-      .putObject({
-        Bucket: process.env.POOL_CACHE_BUCKET_2!,
-        Key: key,
-        Body: JSON.stringify(pools),
-      })
-      .promise()
-
-    metric.putMetric(`${metricPrefix}.s3.latency`, Date.now() - beforeS3)
-
-    log.info({ result }, `Done ${protocol} for ${chainId.toString()}`)
-
-    log.info(`Successfully cached ${chainId} ${protocol} pools to S3`)
-    metric.putMetric(`${metricPrefix}.latency`, Date.now() - beforeAll)
-});
+  log.info(`Successfully cached ${chainId} ${protocol} pools to S3`)
+  metric.putMetric(`${metricPrefix}.latency`, Date.now() - beforeAll)
+})
 
 module.exports = { handler }
