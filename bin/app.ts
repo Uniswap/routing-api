5b350b694cf400f16e626eb48b24820bfcd8df6b
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-05-08 09:05:28 -0700

    tune provisioned concurrency back down to 1 for beta (#658)

--- bin/app.ts
+++ bin/app.ts
@@ -229,7 +229,7 @@ export class RoutingAPIPipeline extends Stack {
       env: { account: '145079444317', region: 'us-east-2' },
       jsonRpcProviders: jsonRpcProviders,
       internalApiKey: internalApiKey.secretValue.toString(),
-      provisionedConcurrency: 10,
+      provisionedConcurrency: 1,
       ethGasStationInfoUrl: ethGasStationInfoUrl.secretValue.toString(),
       stage: STAGE.BETA,
       route53Arn: route53Arn.secretValueFromJson('arn').toString(),
