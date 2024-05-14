e24d01eda122095ade231f48bbc77a7dd1d551fe
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-05-14 09:23:40 -0700

    fix: remove alchemy from the fast endpoint on Sepolia (#685)
    Alchemy provisioned fast endpoint on some chains, but Sepolia is not one of them. We will use Infura as primary provider on Sepolia and Alchemy only as fallback.
    
    On some chains, the API keys will be updated, so we bump lambda version to pick those up from the secrets.

--- bin/stacks/routing-lambda-stack.ts
+++ bin/stacks/routing-lambda-stack.ts
@@ -117,7 +117,7 @@ export class RoutingLambdaStack extends cdk.NestedStack {
 
       description: 'Routing Lambda',
       environment: {
-        VERSION: '17',
+        VERSION: '18',
         NODE_OPTIONS: '--enable-source-maps',
         POOL_CACHE_BUCKET: poolCacheBucket.bucketName,
         POOL_CACHE_BUCKET_2: poolCacheBucket2.bucketName,
