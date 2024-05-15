00566320b61aa9db0e37d9e5df1fe5d7486a5f05
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-05-15 12:07:36 -0700

    chore: update lambda version to pick up alchemy sepolia secret

--- bin/stacks/routing-lambda-stack.ts
+++ bin/stacks/routing-lambda-stack.ts
@@ -117,7 +117,7 @@ export class RoutingLambdaStack extends cdk.NestedStack {
 
       description: 'Routing Lambda',
       environment: {
-        VERSION: '18',
+        VERSION: '19',
         NODE_OPTIONS: '--enable-source-maps',
         POOL_CACHE_BUCKET: poolCacheBucket.bucketName,
         POOL_CACHE_BUCKET_2: poolCacheBucket2.bucketName,
