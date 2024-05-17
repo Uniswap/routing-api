5d982f16029f3fd025e0549bfc1cb10993c6e220
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-05-17 08:10:35 -0700

    feat: introduce quicknode reth on mainnet for shadow latency evaluation

--- bin/app.ts
+++ bin/app.ts
@@ -213,6 +213,7 @@ export class RoutingAPIPipeline extends Stack {
       'QUICKNODE_1',
       'NIRVANA_1',
       'ALCHEMY_1',
+      'QUICKNODE_RETH_1',
       // Blast
       'QUICKNODE_81457',
       'INFURA_81457',
