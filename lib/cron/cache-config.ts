d8da6c9dea0f0200b1a674d4a372cbf51e01ad2d
Miguel Cervera <miguel.cervera@uniswap.org>
2024-05-17 09:50:39 -0700

    chore(cron): Reduce untracked threshold (#700)
    Reduce V2Untracked threshold

--- lib/cron/cache-config.ts
+++ lib/cron/cache-config.ts
@@ -48,7 +48,7 @@ const v3TrackedEthThreshold = 0.01 // Pools need at least 0.01 of trackedEth to 
 const v3UntrackedUsdThreshold = 25000 // Pools need at least 25K USD (untracked) to be selected (for metrics only)
 
 const v2TrackedEthThreshold = 0.025 // Pairs need at least 0.025 of trackedEth to be selected
-const v2UntrackedUsdThreshold = 5000 // Pairs need at least 5K USD (untracked) to be selected (for metrics only)
+const v2UntrackedUsdThreshold = 1000 // Pairs need at least 1K USD (untracked) to be selected (for metrics only)
 
 export const chainProtocols = [
   // V3.
