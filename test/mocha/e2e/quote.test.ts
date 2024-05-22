ffef2e044652be2f3b41a18e3c331c5552cd42ee
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-05-21 22:07:59 -0700

    remove infura from configs

--- test/mocha/e2e/quote.test.ts
+++ test/mocha/e2e/quote.test.ts
@@ -2620,8 +2620,9 @@ describe('quote', function () {
             return
           }
 
-          if (chain === ChainId.BLAST) {
+          if (chain === ChainId.BLAST || chain === ChainId.ZORA) {
             // Blast doesn't have DAI or USDC yet
+            // Zora doesn't have DAI
             return
           }
 
