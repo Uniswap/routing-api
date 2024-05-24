ca1bb3dc44972777ab3deaccbe43fe1126dae1e7
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-05-24 11:14:43 -0700

    fix: rpc gateway dashboard missing latencies (#709)
    Providers' latencies are missing after https://github.com/Uniswap/routing-api/pull/691:
    ![Screenshot 2024-05-24 at 10.18.08 AM.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/BB54fKe6Y10GvrrdKXQN/f363ffe3-496a-4fb7-b64f-bfcdcc1dd283.png)
    
    We will mesh together different call types together into the same chart:
    ![Screenshot 2024-05-24 at 10.17.36 AM.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/BB54fKe6Y10GvrrdKXQN/d407a2b6-1587-4e55-bb76-d25546c918ca.png)
    
    This way we can leverage the existing RPC gateway dashboard to have a single stop shop for all providers' latencies metrics.

--- bin/stacks/rpc-gateway-dashboard.ts
+++ bin/stacks/rpc-gateway-dashboard.ts
@@ -4,7 +4,7 @@ import * as aws_cloudwatch from 'aws-cdk-lib/aws-cloudwatch'
 import { Construct } from 'constructs'
 import _ from 'lodash'
 import { ID_TO_NETWORK_NAME } from '@uniswap/smart-order-router/build/main/util/chains'
-import { MAJOR_METHOD_NAMES } from '../../lib/rpc/SingleJsonRpcProvider'
+import { CallType, MAJOR_METHOD_NAMES } from '../../lib/rpc/SingleJsonRpcProvider'
 import { SUPPORTED_CHAINS } from '../../lib/handlers/injector-sor'
 import { TESTNETS } from '../../lib/util/testNets'
 import { getRpcGatewayEnabledChains } from '../../lib/rpc/ProdConfig'
@@ -97,16 +97,18 @@ function getLatencyMetricsForChain(chainId: ChainId) {
   const metrics = []
   for (const providerName of getProviderNameForChain(chainId)) {
     for (const methodName of MAJOR_METHOD_NAMES) {
-      metrics.push([
-        'Uniswap',
-        `RPC_GATEWAY_${chainId}_${providerName}_evaluated_latency_${methodName}`,
-        'Service',
-        'RoutingAPI',
-        {
-          id: `${methodName}_latency_${chainId}_${providerName}`,
-          label: `${providerName} ${methodName} latency on ${ID_TO_NETWORK_NAME(chainId)}`,
-        },
-      ])
+      for (const callType in CallType) {
+        metrics.push([
+          'Uniswap',
+          `RPC_GATEWAY_${chainId}_${providerName}_evaluated_${CallType[callType]}_latency_${methodName}`,
+          'Service',
+          'RoutingAPI',
+          {
+            id: `${methodName}_latency_${chainId}_${providerName}`,
+            label: `${providerName} ${methodName} ${callType} latency on ${ID_TO_NETWORK_NAME(chainId)}`,
+          },
+        ])
+      }
     }
   }
   return metrics
@@ -214,23 +216,6 @@ function getDbSyncFailMetricsForChain(chainId: ChainId) {
       {
         id: `db_sync_fail_${chainId}_${providerName}`,
         label: `${providerName} db sync fail ${ID_TO_NETWORK_NAME(chainId)}`,
-      },
-    ])
-  }
-  return metrics
-}
-
-function getEvaluateLatencyMetricsForChain(chainId: ChainId) {
-  const metrics = []
-  for (const providerName of getProviderNameForChain(chainId)) {
-    metrics.push([
-      'Uniswap',
-      `RPC_GATEWAY_${chainId}_${providerName}_evaluate_latency`,
-      'Service',
-      'RoutingAPI',
-      {
-        id: `evaluate_latency_${chainId}_${providerName}`,
-        label: `${providerName} (Shadow) Evaluate latency for ${ID_TO_NETWORK_NAME(chainId)}`,
       },
     ])
   }
@@ -814,27 +799,6 @@ export class RpcGatewayDashboardStack extends cdk.NestedStack {
           stat: 'Sum',
           period: 300,
           title: `DB sync fail for ${ID_TO_NETWORK_NAME(chainId)}`,
-          setPeriodToTimeRange: true,
-          yAxis: {
-            left: {
-              showUnits: false,
-              label: 'Occurrences',
-            },
-          },
-        },
-      },
-      {
-        height: 8,
-        width: 24,
-        type: 'metric',
-        properties: {
-          metrics: getEvaluateLatencyMetricsForChain(chainId),
-          view: 'timeSeries',
-          stacked: false,
-          region,
-          stat: 'Sum',
-          period: 300,
-          title: `(Shadow) Evaluate latency call for ${ID_TO_NETWORK_NAME(chainId)}`,
           setPeriodToTimeRange: true,
           yAxis: {
             left: {
