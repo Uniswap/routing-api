b02051032b9fa7f65d0e676963f4e1a78e97ae86
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-06-06 15:14:58 -0700

    fix: add quicknodetest_1 into the secret fetch list

--- bin/app.ts
+++ bin/app.ts
@@ -217,6 +217,7 @@ export class RoutingAPIPipeline extends Stack {
       'NIRVANA_1',
       'ALCHEMY_1',
       'QUICKNODERETH_1',
+      'QUICKNODETEST_1',
       // Blast
       'QUICKNODE_81457',
       // 'INFURA_81457',
