4e8ae3042080555def81a68987f6577fdd8cf9ba
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-06-10 17:19:01 -0700

    fix: bump lambda version to pick up new zksync secret

--- bin/stacks/routing-lambda-stack.ts
+++ bin/stacks/routing-lambda-stack.ts
@@ -107,7 +107,7 @@ export class RoutingLambdaStack extends cdk.NestedStack {
       // 11/8/23: URA currently calls the Routing API with a timeout of 10 seconds.
       // Set this lambda's timeout to be slightly lower to give them time to
       // log the response in the event of a failure on our end.
-      timeout: cdk.Duration.seconds(9),
+      timeout: cdk.Duration.seconds(30),
       memorySize: 2560,
       deadLetterQueueEnabled: true,
       bundling: {
@@ -119,7 +119,7 @@ export class RoutingLambdaStack extends cdk.NestedStack {
 
       description: 'Routing Lambda',
       environment: {
-        VERSION: '24',
+        VERSION: '25',
         NODE_OPTIONS: '--enable-source-maps',
         POOL_CACHE_BUCKET: poolCacheBucket.bucketName,
         POOL_CACHE_BUCKET_2: poolCacheBucket2.bucketName,
