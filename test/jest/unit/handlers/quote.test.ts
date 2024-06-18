545fa739cb80ba208f43b07cd45db41f089dd373
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-06-18 16:00:04 -0700

    fix: hardcode gas on zksync

--- test/jest/unit/handlers/quote.test.ts
+++ test/jest/unit/handlers/quote.test.ts
@@ -6,34 +6,40 @@ import { Protocol } from '@uniswap/router-sdk'
 describe('QuoteHandler', () => {
   describe('.protocolsFromRequest', () => {
     it('returns V3 when no protocols are requested', () => {
-      expect(QuoteHandler.protocolsFromRequest(ChainId.MAINNET, undefined, '', undefined, undefined)).toEqual([
+      const isMobileRequest = ['uniswap-ios', 'uniswap-android'].includes('')
+      expect(QuoteHandler.protocolsFromRequest(ChainId.MAINNET, undefined, isMobileRequest, undefined, undefined)).toEqual([
         Protocol.V3,
       ])
     })
 
     it('returns V3 when forceCrossProtocols is false', () => {
-      expect(QuoteHandler.protocolsFromRequest(ChainId.MAINNET, undefined, '', undefined, false)).toEqual([Protocol.V3])
+      const isMobileRequest = ['uniswap-ios', 'uniswap-android'].includes('')
+      expect(QuoteHandler.protocolsFromRequest(ChainId.MAINNET, undefined, isMobileRequest, undefined, false)).toEqual([Protocol.V3])
     })
 
     it('returns empty when forceCrossProtocols is true', () => {
-      expect(QuoteHandler.protocolsFromRequest(ChainId.MAINNET, undefined, '', undefined, true)).toEqual([])
+      const isMobileRequest = ['uniswap-ios', 'uniswap-android'].includes('')
+      expect(QuoteHandler.protocolsFromRequest(ChainId.MAINNET, undefined, isMobileRequest, undefined, true)).toEqual([])
     })
 
     it('returns requested protocols', () => {
+      const isMobileRequest = ['uniswap-ios', 'uniswap-android'].includes('')
       expect(
-        QuoteHandler.protocolsFromRequest(ChainId.MAINNET, ['v2', 'v3', 'mixed'], '', undefined, undefined)
+        QuoteHandler.protocolsFromRequest(ChainId.MAINNET, ['v2', 'v3', 'mixed'], isMobileRequest, undefined, undefined)
       ).toEqual([Protocol.V2, Protocol.V3, Protocol.MIXED])
     })
 
     it('returns a different set of requested protocols', () => {
-      expect(QuoteHandler.protocolsFromRequest(ChainId.MAINNET, ['v3', 'mixed'], '', undefined, undefined)).toEqual([
+      const isMobileRequest = ['uniswap-ios', 'uniswap-android'].includes('')
+      expect(QuoteHandler.protocolsFromRequest(ChainId.MAINNET, ['v3', 'mixed'], isMobileRequest, undefined, undefined)).toEqual([
         Protocol.V3,
         Protocol.MIXED,
       ])
     })
 
     it('works with other chains', () => {
-      expect(QuoteHandler.protocolsFromRequest(ChainId.BASE, ['v2', 'v3', 'mixed'], '', undefined, undefined)).toEqual([
+      const isMobileRequest = ['uniswap-ios', 'uniswap-android'].includes('')
+      expect(QuoteHandler.protocolsFromRequest(ChainId.BASE, ['v2', 'v3', 'mixed'], isMobileRequest, undefined, undefined)).toEqual([
         Protocol.V2,
         Protocol.V3,
         Protocol.MIXED,
@@ -41,28 +47,31 @@ describe('QuoteHandler', () => {
     })
 
     it('returns undefined when a requested protocol is invalid', () => {
+      const isMobileRequest = ['uniswap-ios', 'uniswap-android'].includes('')
       expect(
-        QuoteHandler.protocolsFromRequest(ChainId.BASE, ['v2', 'v3', 'mixed', 'miguel'], '', undefined, undefined)
+        QuoteHandler.protocolsFromRequest(ChainId.BASE, ['v2', 'v3', 'mixed', 'miguel'], isMobileRequest, undefined, undefined)
       ).toBeUndefined()
     })
 
     describe('for mobile request', () => {
       it('removes v2 and mixed with other chains, when the requested source is mobile', () => {
         ;['uniswap-ios', 'uniswap-android'].forEach((requestSource) => {
+          const isMobileRequest = ['uniswap-ios', 'uniswap-android'].includes(requestSource)
           expect(
-            QuoteHandler.protocolsFromRequest(ChainId.BASE, ['v2', 'v3', 'mixed'], requestSource, undefined, undefined)
+            QuoteHandler.protocolsFromRequest(ChainId.BASE, ['v2', 'v3', 'mixed'], isMobileRequest, undefined, undefined)
           ).toEqual([Protocol.V3])
         })
       })
 
       it('removes v2 and mixed with other chains, when the requested source is mobile, and version 1.22, 1.22.5 or 1.23', () => {
         ;['uniswap-ios', 'uniswap-android'].forEach((requestSource) => {
+          const isMobileRequest = ['uniswap-ios', 'uniswap-android'].includes(requestSource)
           ;['1.22', '1.22.5', '1.23'].forEach((appVersion) => {
             expect(
               QuoteHandler.protocolsFromRequest(
                 ChainId.BASE,
                 ['v2', 'v3', 'mixed'],
-                requestSource,
+                isMobileRequest,
                 appVersion,
                 undefined
               )
@@ -73,12 +82,13 @@ describe('QuoteHandler', () => {
 
       it('allows v2 and mixed with mainnet, even when the requested source is mobile, and version 1.22, 1.22.5 or 1.23', () => {
         ;['uniswap-ios', 'uniswap-android'].forEach((requestSource) => {
+          const isMobileRequest = ['uniswap-ios', 'uniswap-android'].includes(requestSource)
           ;['1.22', '1.22.5', '1.23', '1.23.build-0'].forEach((appVersion) => {
             expect(
               QuoteHandler.protocolsFromRequest(
                 ChainId.MAINNET,
                 ['v2', 'v3', 'mixed'],
-                requestSource,
+                isMobileRequest,
                 appVersion,
                 undefined
               )
@@ -89,12 +99,13 @@ describe('QuoteHandler', () => {
 
       it('allows v2 and mixed with other chains, when the requested source is mobile, and version is 1.24 or greater', () => {
         ;['uniswap-ios', 'uniswap-android'].forEach((requestSource) => {
+          const isMobileRequest = ['uniswap-ios', 'uniswap-android'].includes(requestSource)
           ;['1.24', '1.25', '1.25.5', '1.26', '1.26.test'].forEach((appVersion) => {
             expect(
               QuoteHandler.protocolsFromRequest(
                 ChainId.BASE,
                 ['v2', 'v3', 'mixed'],
-                requestSource,
+                isMobileRequest,
                 appVersion,
                 undefined
               )
