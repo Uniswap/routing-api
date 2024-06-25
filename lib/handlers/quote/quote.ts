68c4ff90e72d1c270eb5a80a4df83e8c0d6f151a
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-06-25 11:20:28 -0700

    fix: celo high gas limit

--- lib/handlers/quote/quote.ts
+++ lib/handlers/quote/quote.ts
@@ -35,7 +35,7 @@ import { SwapOptionsFactory } from './SwapOptionsFactory'
 import { GlobalRpcProviders } from '../../rpc/GlobalRpcProviders'
 import semver from 'semver'
 import { BigNumber } from '@ethersproject/bignumber'
-import { ZKSYNC_UPPER_SWAP_GAS_LIMIT } from '../../util/gasLimit'
+import { CELO_UPPER_SWAP_GAS_LIMIT, ZKSYNC_UPPER_SWAP_GAS_LIMIT } from '../../util/gasLimit'
 
 export class QuoteHandler extends APIGLambdaHandler<
   ContainerInjected,
@@ -814,6 +814,14 @@ export class QuoteHandler extends APIGLambdaHandler<
         }
 
         return ZKSYNC_UPPER_SWAP_GAS_LIMIT
+      case ChainId.CELO:
+        if (estimatedGasUsed.gt(CELO_UPPER_SWAP_GAS_LIMIT)) {
+          // this is a check to ensure that we don't return the gas used smaller than upper swap gas limit,
+          // although this is unlikely
+          return estimatedGasUsed
+        }
+
+        return CELO_UPPER_SWAP_GAS_LIMIT
       default:
         return estimatedGasUsed
     }
@@ -842,6 +850,18 @@ export class QuoteHandler extends APIGLambdaHandler<
           JSBI.BigInt(estimatedGasUsed)
         )
         return CurrencyAmount.fromRawAmount(estimatedGasUsedUSD.currency, correctedEstimateGasUsedUSD)
+      case ChainId.CELO:
+        if (estimatedGasUsed.gt(CELO_UPPER_SWAP_GAS_LIMIT)) {
+          // this is a check to ensure that we don't return the gas used smaller than upper swap gas limit,
+          // although this is unlikely
+          return estimatedGasUsedUSD
+        }
+
+        const correctedEstimateGasUsedUSDCELO = JSBI.divide(
+          JSBI.multiply(estimatedGasUsedUSD.quotient, JSBI.BigInt(CELO_UPPER_SWAP_GAS_LIMIT)),
+          JSBI.BigInt(estimatedGasUsed)
+        )
+        return CurrencyAmount.fromRawAmount(estimatedGasUsedUSD.currency, correctedEstimateGasUsedUSDCELO)
       default:
         return estimatedGasUsedUSD
     }
--- lib/util/gasLimit.ts
+++ lib/util/gasLimit.ts
@@ -1,3 +1,5 @@
 import { BigNumber } from '@ethersproject/bignumber'
 
 export const ZKSYNC_UPPER_SWAP_GAS_LIMIT = BigNumber.from(6000000)
+// CELO high gas limit from SOR https://github.com/Uniswap/smart-order-router/blob/main/src/routers/alpha-router/alpha-router.ts#L670
+export const CELO_UPPER_SWAP_GAS_LIMIT = BigNumber.from(5000000)
