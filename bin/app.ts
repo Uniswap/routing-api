63de73858911b4c762c278c82c91dab7fc90e943
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-10-02 09:14:08 -0700

    fix: bump lambda version to fix missing RPC URL

--- bin/app.ts
+++ bin/app.ts
@@ -365,6 +365,8 @@ const jsonRpcProviders = {
   WEB3_RPC_56: process.env.WEB3_RPC_56!,
   WEB3_RPC_8453: process.env.WEB3_RPC_8453!,
   WEB3_RPC_324: process.env.WEB3_RPC_324!,
+  WEB3_RPC_480: process.env.WEB3_RPC_480!,
+  WEB3_RPC_1301: process.env.WEB3_RPC_1301!,
   // The followings are for RPC Gateway
   // Optimism
   // INFURA_10: process.env.INFURA_10!,
