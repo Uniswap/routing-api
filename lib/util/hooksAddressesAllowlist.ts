2043f0d04f65261a7df0bc186a498f02f67818e0
Siyu Jiang (See-You John) <91580504+jsy1218@users.noreply.github.com>
2024-10-18 11:13:11 -0700

    fix: filter out non-zero hooks address during v4 subgraph cron job (#879)
    * fix: filter out non-zero hooks address during v4 subgraph cron job
    
    * fix prettier
    
    * add extraHooksAddressesOnSepolia

---
+++ lib/util/hooksAddressesAllowlist.ts
@@ -0,0 +1,35 @@
+import { ChainId } from '@uniswap/sdk-core'
+import { ADDRESS_ZERO } from '@uniswap/router-sdk'
+
+export const extraHooksAddressesOnSepolia = '0x0000000000000000000000000000000000000020'
+
+// we do not allow v4 pools with non-zero hook address to be routed through in the initial v4 launch.
+// this is the ultimate safeguard in the routing subgraph pool cron job.
+export const HOOKS_ADDRESSES_ALLOWLIST: { [chain in ChainId]: Array<string> } = {
+  [ChainId.MAINNET]: [ADDRESS_ZERO],
+  [ChainId.GOERLI]: [ADDRESS_ZERO],
+  [ChainId.SEPOLIA]: [ADDRESS_ZERO, extraHooksAddressesOnSepolia],
+  [ChainId.OPTIMISM]: [ADDRESS_ZERO],
+  [ChainId.OPTIMISM_GOERLI]: [ADDRESS_ZERO],
+  [ChainId.OPTIMISM_SEPOLIA]: [ADDRESS_ZERO],
+  [ChainId.ARBITRUM_ONE]: [ADDRESS_ZERO],
+  [ChainId.ARBITRUM_GOERLI]: [ADDRESS_ZERO],
+  [ChainId.ARBITRUM_SEPOLIA]: [ADDRESS_ZERO],
+  [ChainId.POLYGON]: [ADDRESS_ZERO],
+  [ChainId.POLYGON_MUMBAI]: [ADDRESS_ZERO],
+  [ChainId.CELO]: [ADDRESS_ZERO],
+  [ChainId.CELO_ALFAJORES]: [ADDRESS_ZERO],
+  [ChainId.GNOSIS]: [ADDRESS_ZERO],
+  [ChainId.MOONBEAM]: [ADDRESS_ZERO],
+  [ChainId.BNB]: [ADDRESS_ZERO],
+  [ChainId.AVALANCHE]: [ADDRESS_ZERO],
+  [ChainId.BASE_GOERLI]: [ADDRESS_ZERO],
+  [ChainId.BASE]: [ADDRESS_ZERO],
+  [ChainId.ZORA]: [ADDRESS_ZERO],
+  [ChainId.ZORA_SEPOLIA]: [ADDRESS_ZERO],
+  [ChainId.ROOTSTOCK]: [ADDRESS_ZERO],
+  [ChainId.BLAST]: [ADDRESS_ZERO],
+  [ChainId.ZKSYNC]: [ADDRESS_ZERO],
+  [ChainId.WORLDCHAIN]: [ADDRESS_ZERO],
+  [ChainId.ASTROCHAIN_SEPOLIA]: [ADDRESS_ZERO],
+}
