5b094b2234168cad2e2913d4a9241a939f5c850c
Vasilis Xouris <vasileios.xouris@uniswap.org>
2024-10-22 11:42:11 -0400

    fix(rpc): set timeout to 9s (default was 120s)

--- lib/rpc/SingleJsonRpcProvider.ts
+++ lib/rpc/SingleJsonRpcProvider.ts
@@ -73,6 +73,7 @@ export class SingleJsonRpcProvider extends StaticJsonRpcProvider {
       {
         url: url,
         headers: headers,
+        timeout: 8000 // 8 seconds (default is 120s)
       },
       network
     )
