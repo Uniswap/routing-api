beddc064527952de6440bd0eac91611aa0c5b4da
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-10-29 09:23:24 -0400

    1 hour cached routes refresh

--- lib/util/defaultBlocksToLiveRoutesDB.ts
+++ lib/util/defaultBlocksToLiveRoutesDB.ts
@@ -1,48 +1,48 @@
 import { ChainId } from '@uniswap/sdk-core'
 
 // This is not being used in production today anyway, due to below filterExpiredCachedRoutes method not really filtering on the blocks-to-live
-// heuristic is initially within 30 seconds we find a route. (but we are changing to every 10 minutes now)
+// heuristic is initially within 30 seconds we find a route. (but we are changing to every 1 hour now)
 // For the Ratio we are approximating Phi (Golden Ratio) by creating a fraction with 2 consecutive Fibonacci numbers
 
 // changing to this way with ChainId enum as key indexing, so that we wont forgot to add new chain tuned blocks-to-live
 // those are only gonna be enabled with DynamoRouteCachingProvider.newCachedRoutesRolloutPercent anyway
 export const DEFAULT_BLOCKS_TO_LIVE_ROUTES_DB: { [chain in ChainId]: number } = {
-  // (10 minutes) / (12 seconds)= 50
-  [ChainId.MAINNET]: 50,
-  [ChainId.GOERLI]: 50,
-  [ChainId.SEPOLIA]: 50,
-  // (10 minutes) / (2 seconds) = 300
-  [ChainId.OPTIMISM]: 300,
-  [ChainId.OPTIMISM_GOERLI]: 300,
-  [ChainId.OPTIMISM_SEPOLIA]: 300,
-  [ChainId.BASE]: 300,
-  [ChainId.ZORA]: 300,
-  [ChainId.BASE_GOERLI]: 300,
-  [ChainId.ZORA_SEPOLIA]: 300,
-  [ChainId.BLAST]: 300,
-  [ChainId.WORLDCHAIN]: 300,
-  // (10 minutes) / (1 seconds) = 600
-  [ChainId.ASTROCHAIN_SEPOLIA]: 600,
-  // (10 minutes) / (250 milliseconds) = 2400
-  [ChainId.ARBITRUM_ONE]: 2400,
-  [ChainId.ARBITRUM_GOERLI]: 2400,
-  [ChainId.ARBITRUM_SEPOLIA]: 2400,
-  // (10 minutes) / (2 seconds) = 300
-  [ChainId.POLYGON]: 300,
-  [ChainId.POLYGON_MUMBAI]: 300,
-  // (10 minutes) / (5 seconds) = 120
-  [ChainId.CELO]: 120,
-  [ChainId.CELO_ALFAJORES]: 120,
-  // (10 minutes) / (5 seconds) = 120
-  [ChainId.GNOSIS]: 120,
-  // (10 minutes) / (6 seconds) = 100
-  [ChainId.MOONBEAM]: 100,
-  // (10 minutes) / (3 seconds) = 200
-  [ChainId.BNB]: 200,
-  // (10 minutes) / (3 seconds) = 200
-  [ChainId.AVALANCHE]: 200,
-  // (10 minutes) / (33 seconds) = 18
-  [ChainId.ROOTSTOCK]: 18,
-  // (10 minutes) / (1 seconds) = 600
-  [ChainId.ZKSYNC]: 600,
+  // (60 minutes) / (12 seconds)= 300
+  [ChainId.MAINNET]: 300,
+  [ChainId.GOERLI]: 300,
+  [ChainId.SEPOLIA]: 300,
+  // (60 minutes) / (2 seconds) = 1800
+  [ChainId.OPTIMISM]: 1800,
+  [ChainId.OPTIMISM_GOERLI]: 1800,
+  [ChainId.OPTIMISM_SEPOLIA]: 1800,
+  [ChainId.BASE]: 1800,
+  [ChainId.ZORA]: 1800,
+  [ChainId.BASE_GOERLI]: 1800,
+  [ChainId.ZORA_SEPOLIA]: 1800,
+  [ChainId.BLAST]: 1800,
+  [ChainId.WORLDCHAIN]: 1800,
+  // (60 minutes) / (1 seconds) = 3600
+  [ChainId.ASTROCHAIN_SEPOLIA]: 3600,
+  // (60 minutes) / (250 milliseconds) = 14400
+  [ChainId.ARBITRUM_ONE]: 14400,
+  [ChainId.ARBITRUM_GOERLI]: 14400,
+  [ChainId.ARBITRUM_SEPOLIA]: 14400,
+  // (60 minutes) / (2 seconds) = 1800
+  [ChainId.POLYGON]: 1800,
+  [ChainId.POLYGON_MUMBAI]: 1800,
+  // (60 minutes) / (5 seconds) = 720
+  [ChainId.CELO]: 720,
+  [ChainId.CELO_ALFAJORES]: 720,
+  // (60 minutes) / (5 seconds) = 720
+  [ChainId.GNOSIS]: 720,
+  // (60 minutes) / (6 seconds) = 600
+  [ChainId.MOONBEAM]: 600,
+  // (60 minutes) / (3 seconds) = 1200
+  [ChainId.BNB]: 1200,
+  // (60 minutes) / (3 seconds) = 1200
+  [ChainId.AVALANCHE]: 1200,
+  // (60 minutes) / (33 seconds) = 148
+  [ChainId.ROOTSTOCK]: 148,
+  // (60 minutes) / (1 seconds) = 3600
+  [ChainId.ZKSYNC]: 3600,
 }
