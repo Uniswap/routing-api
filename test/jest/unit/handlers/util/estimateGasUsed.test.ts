6bd363ba38875416f666130cc388fc92a848a6ac
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2025-01-17 11:36:53 -0800

    add unit test

--- test/jest/unit/handlers/util/estimateGasUsed.test.ts
+++ test/jest/unit/handlers/util/estimateGasUsed.test.ts
@@ -2,7 +2,11 @@ import { describe, it, expect } from '@jest/globals'
 import { ChainId } from '@uniswap/sdk-core'
 import { adhocCorrectGasUsed } from '../../../../../lib/util/estimateGasUsed'
 import { BigNumber } from '@ethersproject/bignumber'
-import { CELO_UPPER_SWAP_GAS_LIMIT, ZKSYNC_UPPER_SWAP_GAS_LIMIT } from '../../../../../lib/util/gasLimit'
+import {
+  CELO_UPPER_SWAP_GAS_LIMIT,
+  MONAD_UPPER_SWAP_GAS_LIMIT,
+  ZKSYNC_UPPER_SWAP_GAS_LIMIT,
+} from '../../../../../lib/util/gasLimit'
 
 describe('estimateGasUsed', () => {
   it('returns normal gas for mainnet', () => {
@@ -48,5 +52,10 @@ describe('estimateGasUsed', () => {
   it('returns normal gas for celo on extension', () => {
     const estimatedGasUsed = CELO_UPPER_SWAP_GAS_LIMIT.add(1)
     expect(adhocCorrectGasUsed(estimatedGasUsed, ChainId.CELO)).toBe(estimatedGasUsed)
+  })
+
+  it('returns upper limit gas for monad', () => {
+    const estimatedGasUsed = MONAD_UPPER_SWAP_GAS_LIMIT.sub(1)
+    expect(adhocCorrectGasUsed(estimatedGasUsed, ChainId.MONAD_TESTNET)).toBe(MONAD_UPPER_SWAP_GAS_LIMIT)
   })
 })
