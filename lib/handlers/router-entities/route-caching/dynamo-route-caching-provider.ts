fd544aca4d36309adbbedc3e8b72148d11d96ec9
Siyu Jiang (See-You John) <91580504+jsy1218@users.noreply.github.com>
2025-01-29 09:02:46 -0800

    fix: disable v4 cached routes set caching (#984)
    * fix: disable v4 cached routes set caching
    
    * skip unit test

--- lib/handlers/router-entities/route-caching/dynamo-route-caching-provider.ts
+++ lib/handlers/router-entities/route-caching/dynamo-route-caching-provider.ts
@@ -106,7 +106,7 @@ export class DynamoRouteCachingProvider extends IRouteCachingProvider {
     const { currencyIn, currencyOut } = this.determineCurrencyInOut(amount, quoteCurrency, tradeType)
 
     // for getting the cached routes, we dont know if the cached route will contains a v4 pool or not, so we try to see if the input protocols contain v4
-    const includesV4Pool = protocols.includes(Protocol.V4)
+    const includesV4Pool = false // TODO: re-enable v4 protocols.includes(Protocol.V4)
 
     const partitionKey = new PairTradeTypeChainId({
       currencyIn: PairTradeTypeChainId.deriveCurrencyAddress(includesV4Pool, currencyIn),
@@ -338,7 +338,7 @@ export class DynamoRouteCachingProvider extends IRouteCachingProvider {
       // if no Item is found it means we need to send a caching request
       if (shouldSendCachingRequest) {
         metric.putMetric('CachingQuoteForRoutesDbRequestSent', 1, MetricLoggerUnit.Count)
-        this.sendAsyncCachingRequest(partitionKey, [Protocol.V2, Protocol.V3, Protocol.V4, Protocol.MIXED], amount)
+        this.sendAsyncCachingRequest(partitionKey, [Protocol.V2, Protocol.V3, Protocol.MIXED], amount)
         this.setRoutesDbCachingIntentFlag(partitionKey, amount, currentBlockNumber)
       } else {
         metric.putMetric('CachingQuoteForRoutesDbRequestNotNeeded', 1, MetricLoggerUnit.Count)
