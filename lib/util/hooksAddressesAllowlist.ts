998485305a745e453d6ec0ab1b89eb8353dc9649
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2025-02-05 16:26:18 -0800

    fix: add flaunch eth/fleth autowrap hooks

--- lib/util/hooksAddressesAllowlist.ts
+++ lib/util/hooksAddressesAllowlist.ts
@@ -3,6 +3,7 @@ import { ADDRESS_ZERO } from '@uniswap/router-sdk'
 
 export const extraHooksAddressesOnSepolia = '0x0000000000000000000000000000000000000020'
 export const FLAUNCH_HOOKS_ADDRESS_ON_BASE = '0x51Bba15255406Cfe7099a42183302640ba7dAFDC'
+export const ETH_FLETH_AUTO_WRAP_HOOKS_ADDRESS_ON_BASE = '0x9e433f32bb5481a9ca7dff5b3af74a7ed041a888'
 
 // we do not allow v4 pools with non-zero hook address to be routed through in the initial v4 launch.
 // this is the ultimate safeguard in the routing subgraph pool cron job.
@@ -26,7 +27,7 @@ export const HOOKS_ADDRESSES_ALLOWLIST: { [chain in ChainId]: Array<string> } = 
   [ChainId.AVALANCHE]: [ADDRESS_ZERO],
   [ChainId.BASE_GOERLI]: [ADDRESS_ZERO],
   [ChainId.BASE_SEPOLIA]: [ADDRESS_ZERO],
-  [ChainId.BASE]: [ADDRESS_ZERO, FLAUNCH_HOOKS_ADDRESS_ON_BASE],
+  [ChainId.BASE]: [ADDRESS_ZERO, FLAUNCH_HOOKS_ADDRESS_ON_BASE, ETH_FLETH_AUTO_WRAP_HOOKS_ADDRESS_ON_BASE],
   [ChainId.ZORA]: [ADDRESS_ZERO],
   [ChainId.ZORA_SEPOLIA]: [ADDRESS_ZERO],
   [ChainId.ROOTSTOCK]: [ADDRESS_ZERO],
