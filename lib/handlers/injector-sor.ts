a79dc1e7f3d2542e0ad837d0f46b411b4ba4060d
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2025-02-12 10:10:12 -0800

    chore: bump sor to 4.17.17 - fix: add base mixed quoter v2 (#1002)

--- lib/handlers/injector-sor.ts
+++ lib/handlers/injector-sor.ts
@@ -533,7 +533,7 @@ export abstract class InjectorSOR<Router, QueryParams> extends Injector<
             ChainId.MAINNET,
           ]
 
-          const mixedSupported = [ChainId.MAINNET, ChainId.SEPOLIA, ChainId.GOERLI, ChainId.UNICHAIN]
+          const mixedSupported = [ChainId.MAINNET, ChainId.SEPOLIA, ChainId.GOERLI, ChainId.UNICHAIN, ChainId.BASE]
 
           const cachedRoutesCacheInvalidationFixRolloutPercentage = NEW_CACHED_ROUTES_ROLLOUT_PERCENT[chainId]
 
