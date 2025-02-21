1ce43b47779704025fbce5c67fb2eade8fd0a715
Siyu Jiang (See-You John) <91580504+jsy1218@users.noreply.github.com>
2025-02-20 16:19:55 -0800

    chore: bump sor to 4.18.2 - fix: support mixed quote on rest of L2s (#1022)
    Release https://github.com/Uniswap/smart-order-router/pull/831
    
    alll L2 tests passed https://app.warp.dev/block/Q43sdmGDmSdE8SKuB3Eh1m, L1 tests failure cannot be due to this change, because it does not touch any L1 logic at all.

--- lib/handlers/injector-sor.ts
+++ lib/handlers/injector-sor.ts
@@ -533,7 +533,20 @@ export abstract class InjectorSOR<Router, QueryParams> extends Injector<
             ChainId.MAINNET,
           ]
 
-          const mixedSupported = [ChainId.MAINNET, ChainId.SEPOLIA, ChainId.GOERLI, ChainId.UNICHAIN, ChainId.BASE]
+          const mixedSupported = [
+            ChainId.MAINNET,
+            ChainId.SEPOLIA,
+            ChainId.GOERLI,
+            ChainId.BASE,
+            ChainId.UNICHAIN,
+            ChainId.ARBITRUM_ONE,
+            ChainId.POLYGON,
+            ChainId.OPTIMISM,
+            ChainId.AVALANCHE,
+            ChainId.BNB,
+            ChainId.WORLDCHAIN,
+            ChainId.ZORA,
+          ]
 
           const cachedRoutesCacheInvalidationFixRolloutPercentage = NEW_CACHED_ROUTES_ROLLOUT_PERCENT[chainId]
 
