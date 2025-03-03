2f4a10ab3a140cd4d5c46cee0e2ac181ed76621b
Michael Wang <44713145+mzywang@users.noreply.github.com>
2025-03-03 16:17:51 -0500

    fix: increase base and zora v3 timeout (#1037)
    increase base v3 timeout

--- lib/cron/cache-config.ts
+++ lib/cron/cache-config.ts
@@ -212,7 +212,7 @@ export const chainProtocols = [
     provider: new V3SubgraphProvider(
       ChainId.BASE,
       3,
-      90000,
+      360000, // base has more pools than other chains, so we need to increase the timeout
       true,
       v3TrackedEthThreshold,
       v3UntrackedUsdThreshold,
@@ -268,7 +268,7 @@ export const chainProtocols = [
     provider: new V3SubgraphProvider(
       ChainId.ZORA,
       3,
-      90000,
+      360000, // zora has more pools than other chains, so we need to increase the timeout
       true,
       v3TrackedEthThreshold,
       v3UntrackedUsdThreshold,
