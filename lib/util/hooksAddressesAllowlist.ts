9a4ae6494758e8b5dd347de0dd7fa566dab98fac
Siyu Jiang (See-You John) <91580504+jsy1218@users.noreply.github.com>
2025-03-05 11:13:00 -0800

    feat: allowlist bunni hooks across L1, base, arb (#1039)
    * feat: allowlist bunni L1 hooks
    
    * upgrade ubuntu image for semgrep
    
    * add L2 hooks addresses as well
    
    * update bunni hooks addresses according to https://hackmd.io/@zeframlou/By4apSDdke#10
    
    * comment lower case

--- lib/util/hooksAddressesAllowlist.ts
+++ lib/util/hooksAddressesAllowlist.ts
@@ -1,20 +1,40 @@
 import { ChainId } from '@uniswap/sdk-core'
 import { ADDRESS_ZERO } from '@uniswap/router-sdk'
 
+// all hook addresses need to be lower case, since the check in isHooksPoolRoutable assumes lower case
 export const extraHooksAddressesOnSepolia = '0x0000000000000000000000000000000000000020'
-export const FLAUNCH_HOOKS_ADDRESS_ON_BASE = '0x51Bba15255406Cfe7099a42183302640ba7dAFDC'
+export const FLAUNCH_HOOKS_ADDRESS_ON_BASE = '0x51bba15255406cfe7099a42183302640ba7dafdc'
 export const ETH_FLETH_AUTO_WRAP_HOOKS_ADDRESS_ON_BASE = '0x9e433f32bb5481a9ca7dff5b3af74a7ed041a888'
+export const BUNNI_HOOKS_ADDRESS_v1_0_ON_L1 = '0x0010d0d5db05933fa0d9f7038d365e1541a41888'
+export const BUNNI_HOOKS_ADDRESS_V1_1_ON_L1 = '0x0000da5dcd7ec49d6ca5554f7b1ca1ce33fa1888'
+export const BUNNI_HOOKS_ADDRESS_V1_1_1_ON_L1 = '0x0000fe59823933ac763611a69c88f91d45f81888'
+export const BUNNI_HOOKS_ADDRESS_v1_0_ON_BASE = '0x0010d0d5db05933fa0d9f7038d365e1541a41888'
+export const BUNNI_HOOKS_ADDRESS_V1_1_ON_BASE = '0x0000da5dcd7ec49d6ca5554f7b1ca1ce33fa1888'
+export const BUNNI_HOOKS_ADDRESS_V1_1_1_ON_BASE = '0x0000fe59823933ac763611a69c88f91d45f81888'
+export const BUNNI_HOOKS_ADDRESS_v1_0_ON_ARBITRUM = '0x0010d0d5db05933fa0d9f7038d365e1541a41888'
+export const BUNNI_HOOKS_ADDRESS_V1_1_ON_ARBITRUM = '0x0000da5dcd7ec49d6ca5554f7b1ca1ce33fa1888'
+export const BUNNI_HOOKS_ADDRESS_V1_1_1_ON_ARBITRUM = '0x0000fe59823933ac763611a69c88f91d45f81888'
 
 // we do not allow v4 pools with non-zero hook address to be routed through in the initial v4 launch.
 // this is the ultimate safeguard in the routing subgraph pool cron job.
 export const HOOKS_ADDRESSES_ALLOWLIST: { [chain in ChainId]: Array<string> } = {
-  [ChainId.MAINNET]: [ADDRESS_ZERO],
+  [ChainId.MAINNET]: [
+    ADDRESS_ZERO,
+    BUNNI_HOOKS_ADDRESS_v1_0_ON_L1,
+    BUNNI_HOOKS_ADDRESS_V1_1_ON_L1,
+    BUNNI_HOOKS_ADDRESS_V1_1_1_ON_L1,
+  ],
   [ChainId.GOERLI]: [ADDRESS_ZERO],
   [ChainId.SEPOLIA]: [ADDRESS_ZERO, extraHooksAddressesOnSepolia],
   [ChainId.OPTIMISM]: [ADDRESS_ZERO],
   [ChainId.OPTIMISM_GOERLI]: [ADDRESS_ZERO],
   [ChainId.OPTIMISM_SEPOLIA]: [ADDRESS_ZERO],
-  [ChainId.ARBITRUM_ONE]: [ADDRESS_ZERO],
+  [ChainId.ARBITRUM_ONE]: [
+    ADDRESS_ZERO,
+    BUNNI_HOOKS_ADDRESS_v1_0_ON_ARBITRUM,
+    BUNNI_HOOKS_ADDRESS_V1_1_ON_BASE,
+    BUNNI_HOOKS_ADDRESS_V1_1_1_ON_ARBITRUM,
+  ],
   [ChainId.ARBITRUM_GOERLI]: [ADDRESS_ZERO],
   [ChainId.ARBITRUM_SEPOLIA]: [ADDRESS_ZERO],
   [ChainId.POLYGON]: [ADDRESS_ZERO],
@@ -27,7 +47,14 @@ export const HOOKS_ADDRESSES_ALLOWLIST: { [chain in ChainId]: Array<string> } = 
   [ChainId.AVALANCHE]: [ADDRESS_ZERO],
   [ChainId.BASE_GOERLI]: [ADDRESS_ZERO],
   [ChainId.BASE_SEPOLIA]: [ADDRESS_ZERO],
-  [ChainId.BASE]: [ADDRESS_ZERO, FLAUNCH_HOOKS_ADDRESS_ON_BASE, ETH_FLETH_AUTO_WRAP_HOOKS_ADDRESS_ON_BASE],
+  [ChainId.BASE]: [
+    ADDRESS_ZERO,
+    FLAUNCH_HOOKS_ADDRESS_ON_BASE,
+    ETH_FLETH_AUTO_WRAP_HOOKS_ADDRESS_ON_BASE,
+    BUNNI_HOOKS_ADDRESS_v1_0_ON_BASE,
+    BUNNI_HOOKS_ADDRESS_V1_1_ON_ARBITRUM,
+    BUNNI_HOOKS_ADDRESS_V1_1_1_ON_BASE,
+  ],
   [ChainId.ZORA]: [ADDRESS_ZERO],
   [ChainId.ZORA_SEPOLIA]: [ADDRESS_ZERO],
   [ChainId.ROOTSTOCK]: [ADDRESS_ZERO],
