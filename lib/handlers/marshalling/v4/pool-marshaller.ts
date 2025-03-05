a5fb37794584edfa389a47b984f42e247b444ac4
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2025-03-05 14:29:47 -0800

    fix import

--- lib/handlers/marshalling/v4/pool-marshaller.ts
+++ lib/handlers/marshalling/v4/pool-marshaller.ts
@@ -1,7 +1,7 @@
 import { Pool } from '@uniswap/v4-sdk'
-import { FeeAmount } from '@uniswap/v3-sdk/dist/constants'
 import { MarshalledCurrency, TokenMarshaller } from '../token-marshaller'
 import { Protocol } from '@uniswap/router-sdk'
+import { FeeAmount } from '@uniswap/v3-sdk'
 
 export interface MarshalledPool {
   protocol: Protocol
