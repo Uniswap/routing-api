737504f913b650fecf1612a9c4fb6eb74b57fbce
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2025-03-31 15:58:06 -0700

    feedbacks

--- lib/util/computeProtocolsInvolvedIfMixed.ts
+++ lib/util/computeProtocolsInvolvedIfMixed.ts
@@ -1,27 +1,32 @@
-import { CachedRoute, SupportedRoutes } from '@uniswap/smart-order-router'
+import { CachedRoute, log, SupportedRoutes } from '@uniswap/smart-order-router'
 import { Protocol } from '@uniswap/router-sdk'
 import { Pair } from '@uniswap/v2-sdk'
 import { Pool as V3Pool } from '@uniswap/v3-sdk'
 import { Pool as V4Pool } from '@uniswap/v4-sdk'
 
-export function computeProtocolsInvolvedIfMixed(route: CachedRoute<SupportedRoutes>): Protocol[] {
+export function computeProtocolsInvolvedIfMixed(route: CachedRoute<SupportedRoutes>): Set<Protocol> {
   if (route.route.protocol === Protocol.MIXED) {
-    return route.route.pools.map((pool) => {
-      if (pool instanceof Pair) {
-        return Protocol.V2
-      }
+    return new Set(
+      route.route.pools.map((pool) => {
+        if (pool instanceof Pair) {
+          return Protocol.V2
+        }
 
-      if (pool instanceof V3Pool) {
-        return Protocol.V3
-      }
+        if (pool instanceof V3Pool) {
+          return Protocol.V3
+        }
 
-      if (pool instanceof V4Pool) {
+        if (pool instanceof V4Pool) {
+          return Protocol.V4
+        }
+
+        log.error('Unknown pool type. we will just return Protocol.V4.', pool)
+
+        // default is Protocol.V4
         return Protocol.V4
-      }
-
-      throw new Error(`Unsupported pool type ${JSON.stringify(pool)}`)
-    })
+      })
+    )
   } else {
-    return []
+    return new Set()
   }
 }
