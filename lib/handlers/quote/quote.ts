d15e829b513fd7d18c3cb71d501229a6df5a9453
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2025-04-16 13:41:03 -0700

    increase mixed latency fix to 50%

--- lib/handlers/quote/quote.ts
+++ lib/handlers/quote/quote.ts
@@ -353,7 +353,7 @@ export class QuoteHandler extends APIGLambdaHandler<
       ...(excludedProtocolsFromMixed ? { excludedProtocolsFromMixed } : {}),
       shouldEnableMixedRouteEthWeth: shouldEnableMixedRouteEthWeth,
       ...(cachedRoutesRouteIds ? { cachedRoutesRouteIds } : {}),
-      enableMixedRouteWithUR1_2: 10 >= Math.random() * 100, // enable mixed route with UR v1.2 fix at 1%, to see whether we see quote endpoint perf improvement.
+      enableMixedRouteWithUR1_2: 50 >= Math.random() * 100, // enable mixed route with UR v1.2 fix at 50%, to see whether we see quote endpoint perf improvement.
     }
 
     metric.putMetric(`${intent}Intent`, 1, MetricLoggerUnit.Count)
