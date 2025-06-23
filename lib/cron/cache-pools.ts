f09bc4e3e4f7025e402ebf4b3de450f2406f1745
Siyu Jiang (See-You John) <91580504+jsy1218@users.noreply.github.com>
2025-06-23 10:06:21 -0700

    fix: inflate euler pool TVL (#1143)
    inflate euler pool TVL so that they have the chance to get picked up. euler factory uses JIT liquidity, so subgraph TVL will be zero for euler pools
    
    tested with the same query string param that caused [error](https://us-east-2.console.aws.amazon.com/cloudwatch/home?region=us-east-2#logsV2:logs-insights$3FqueryDetail$3D~(end~0~start~-604800~timeType~'RELATIVE~tz~'LOCAL~unit~'seconds~editorString~'fields*20*40timestamp*2c*20*40message*2c*20*40logStream*2c*20*40log*0a*7c*20filter*20*40message*20like*20*27579ce23c-1109-4309-8c04-42171f55d021*27*0a*7c*20sort*20*40timestamp*20desc*0a*7c*20limit*2010000~queryId~'27f70090-0d6c-4aee-9847-1ff4039c9cce~source~(~'*2faws*2flambda*2fprod-us-east-2-RoutingAPI-Ro-RoutingLambdaF7AD8A1A-erFTueZgLnZ1~'*2faws*2flambda*2fprod-us-east-2-RoutingAPI-R-RoutingLambda2C4DF0900-Bbcx5MI4vlM6)~lang~'CWLI)) in prod, and curl works now https://app.warp.dev/block/zIKywlMbaJh5yufitwXmxZ

--- lib/cron/cache-pools.ts
+++ lib/cron/cache-pools.ts
@@ -44,6 +44,9 @@ const handler: ScheduledHandler = metricScope((metrics) => async (event: EventBr
   const provider = chainProtocols.find(
     (element) => element.protocol == protocol && element.chainId == chainId
   )!.provider
+  const eulerHooksProvider = chainProtocols.find(
+    (element) => element.protocol === Protocol.V4 && element.chainId === chainId
+  )!.eulerHooksProvider
   const log: Logger = bunyan.createLogger({
     name: 'RoutingLambda',
     serializers: bunyan.stdSerializers,
@@ -273,6 +276,30 @@ const handler: ScheduledHandler = metricScope((metrics) => async (event: EventBr
           tvlUSD: 95050.95363442908526427214106054717,
         } as V4SubgraphPool,
       ]
+
+      const eulerHooks = await eulerHooksProvider?.getHooks()
+      if (eulerHooks) {
+        metric.putMetric('eulerHooks.length', eulerHooks.length)
+
+        const eulerPools = await Promise.all(
+          eulerHooks.map(async (eulerHook) => {
+            const pool = await eulerHooksProvider?.getPoolByHook(eulerHook.hook)
+            log.info(`eulerHooks pool ${JSON.stringify(pool)}`)
+
+            // we need to inflate euler pool TVL from 0 to significant TVL, so that they have a chance to be picked up
+            ;(pool as V4SubgraphPool).tvlUSD = 1000
+            ;(pool as V4SubgraphPool).tvlETH = 5500000
+
+            return pool
+          })
+        )
+
+        eulerPools.forEach((pool) => {
+          if (pool) {
+            manuallyIncludedV4Pools.push(pool as V4SubgraphPool)
+          }
+        })
+      }
 
       if (chainId === ChainId.MAINNET) {
         // https://bunni.xyz/explore/pools/ethereum/0x9148f00424c4b40a9ec4b03912f091138e9e91a60980550ed97ed7f9dc998cb5
