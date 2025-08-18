78b7a72169131010bc205ae45fad55f05a2f52f0
Siyu Jiang (See-You John) <91580504+jsy1218@users.noreply.github.com>
2025-08-18 15:57:49 -0700

    fix: add error logging for decimal parsing as well as fallback to 18 decimals (#1193)
    we notice 4 zip files are stuck because of this decimal invariant:
    
    ```
        {
            "@timestamp": "2025-08-18 17:16:21.366",
            "@message": {
                "name": "RoutingLambda",
                "requestId": "fb2d5898-4a35-ccd9-09d3-e218de482890",
                "hostname": "169.254.58.197",
                "pid": 13,
                "level": 50,
                "err": {
                    "message": "Invariant failed: DECIMALS",
                    "name": "Error",
                    "stack": "Error: Invariant failed: DECIMALS\n    at ixt (/var/task/index.js:1:28406)\n    at e.xae (/var/task/index.js:1:221586)\n    at new e (/var/task/index.js:1:222069)\n    at wI0 (/var/task/index.js:998:28679)\n    at /var/task/index.js:998:29118\n    at Array.forEach (<anonymous>)\n    at Hmt (/var/task/index.js:998:29103)\n    at /var/task/index.js:998:39764\n    at process.processTicksAndRejections (node:internal/process/task_queues:95:5)"
                },
                "msg": "Failed to get pools for V4 on 1",
                "time": "2025-08-18T17:16:21.363Z",
                "v": 0
            }
        },
    ```
    
    we are falling back to use 18 decimals

--- lib/util/v4HooksPoolsFiltering.ts
+++ lib/util/v4HooksPoolsFiltering.ts
@@ -14,30 +14,63 @@ function convertV4PoolToGroupingKey(pool: V4SubgraphPool): V4PoolGroupingKey {
 }
 
 function isHooksPoolRoutable(pool: V4SubgraphPool, chainId: ChainId): boolean {
-  const tokenA: Currency =
-    pool.token0.id === ADDRESS_ZERO
-      ? nativeOnChain(chainId)
-      : new Token(chainId, pool.token0.id, parseInt(pool.token0.decimals), pool.token0.symbol, pool.token0.name)
-  const tokenB: Currency =
-    pool.token1.id === ADDRESS_ZERO
-      ? nativeOnChain(chainId)
-      : new Token(chainId, pool.token1.id, parseInt(pool.token1.decimals), pool.token1.symbol, pool.token1.name)
+  try {
+    const tokenA: Currency =
+      pool.token0.id === ADDRESS_ZERO
+        ? nativeOnChain(chainId)
+        : new Token(chainId, pool.token0.id, parseInt(pool.token0.decimals), pool.token0.symbol, pool.token0.name)
+    const tokenB: Currency =
+      pool.token1.id === ADDRESS_ZERO
+        ? nativeOnChain(chainId)
+        : new Token(chainId, pool.token1.id, parseInt(pool.token1.decimals), pool.token1.symbol, pool.token1.name)
 
-  return (
-    // if hook address is ADDRESS_ZERO, it means the pool is not a hooks pool
-    pool.hooks === ADDRESS_ZERO ||
-    (!Hook.hasSwapPermissions(pool.hooks) &&
-      // If the fee tier is smaller than or equal to 100%, it means the pool is not dynamic fee pool.
-      // Swap fee in total can be 100% (https://github.com/Uniswap/v4-core/blob/b619b6718e31aa5b4fa0286520c455ceb950276d/src/libraries/SwapMath.sol#L12)
-      // Dynamic fee is at 0x800000 or 838.8608% fee tier.
-      // Since pool manager doesn;t check the fee at 100% max during pool initialization (https://github.com/Uniswap/v4-core/blob/main/src/PoolManager.sol#L128)
-      // it's more defensive programming to ensure the fee tier is less than or equal to 100%
-      Number(pool.feeTier) <= 1000000 &&
-      // ROUTE-606: Non-allowlisted hooks might make it in routing if dynamic fee
-      // there's a chance dynamic fee has been updated to be <= 100%, but it's still a dyanmic fee hooked pool
-      // in this case, the only way to track is to backtrack the computed pool id with 838% fee tier with the current pool id
-      !isPoolFeeDynamic(tokenA, tokenB, pool))
-  )
+    return (
+      // if hook address is ADDRESS_ZERO, it means the pool is not a hooks pool
+      pool.hooks === ADDRESS_ZERO ||
+      (!Hook.hasSwapPermissions(pool.hooks) &&
+        // If the fee tier is smaller than or equal to 100%, it means the pool is not dynamic fee pool.
+        // Swap fee in total can be 100% (https://github.com/Uniswap/v4-core/blob/b619b6718e31aa5b4fa0286520c455ceb950276d/src/libraries/SwapMath.sol#L12)
+        // Dynamic fee is at 0x800000 or 838.8608% fee tier.
+        // Since pool manager doesn;t check the fee at 100% max during pool initialization (https://github.com/Uniswap/v4-core/blob/main/src/PoolManager.sol#L128)
+        // it's more defensive programming to ensure the fee tier is less than or equal to 100%
+        Number(pool.feeTier) <= 1000000 &&
+        // ROUTE-606: Non-allowlisted hooks might make it in routing if dynamic fee
+        // there's a chance dynamic fee has been updated to be <= 100%, but it's still a dyanmic fee hooked pool
+        // in this case, the only way to track is to backtrack the computed pool id with 838% fee tier with the current pool id
+        !isPoolFeeDynamic(tokenA, tokenB, pool))
+    )
+  } catch (e) {
+    log.error(
+      `Error creating tokens for pool ${pool.id} on chain ${chainId} with token0 decimals ${pool.token0.decimals} token1 decimals ${pool.token1.decimals}: ${e}`
+    )
+
+    // hardcode to 18 decimals since we cannot parse and pass the token invariant checks
+    const tokenA: Currency =
+      pool.token0.id === ADDRESS_ZERO
+        ? nativeOnChain(chainId)
+        : new Token(chainId, pool.token0.id, 18, pool.token0.symbol, pool.token0.name)
+    // hardcode to 18 decimals since we cannot parse and pass the token invariant checks
+    const tokenB: Currency =
+      pool.token1.id === ADDRESS_ZERO
+        ? nativeOnChain(chainId)
+        : new Token(chainId, pool.token1.id, 18, pool.token1.symbol, pool.token1.name)
+
+    return (
+      // if hook address is ADDRESS_ZERO, it means the pool is not a hooks pool
+      pool.hooks === ADDRESS_ZERO ||
+      (!Hook.hasSwapPermissions(pool.hooks) &&
+        // If the fee tier is smaller than or equal to 100%, it means the pool is not dynamic fee pool.
+        // Swap fee in total can be 100% (https://github.com/Uniswap/v4-core/blob/b619b6718e31aa5b4fa0286520c455ceb950276d/src/libraries/SwapMath.sol#L12)
+        // Dynamic fee is at 0x800000 or 838.8608% fee tier.
+        // Since pool manager doesn;t check the fee at 100% max during pool initialization (https://github.com/Uniswap/v4-core/blob/main/src/PoolManager.sol#L128)
+        // it's more defensive programming to ensure the fee tier is less than or equal to 100%
+        Number(pool.feeTier) <= 1000000 &&
+        // ROUTE-606: Non-allowlisted hooks might make it in routing if dynamic fee
+        // there's a chance dynamic fee has been updated to be <= 100%, but it's still a dyanmic fee hooked pool
+        // in this case, the only way to track is to backtrack the computed pool id with 838% fee tier with the current pool id
+        !isPoolFeeDynamic(tokenA, tokenB, pool))
+    )
+  }
 }
 
 // it has to be a min heap in order to preserve the top eth tvl v4 pools
