a6ee034cced624ea50c2ed6cad2cc17c8ed1b9cb
Alex Francisci <alex.francisci@gmail.com>
2025-09-22 15:35:49 -0400

    fixing line return issue

--- lib/util/v4HooksPoolsFiltering.ts
+++ lib/util/v4HooksPoolsFiltering.ts
@@ -158,8 +158,7 @@ export function v4HooksPoolsFiltering(chainId: ChainId, pools: Array<V4SubgraphP
 
       let shouldNotAddV4Pool = false
 
-      const isZoraPool =
-        pool.hooks.toLowerCase().includes('ZORA_CREATOR') || pool.hooks.toLowerCase().includes('ZORA_POST') &&
+      const isZoraPool = pool.hooks.toLowerCase().includes('ZORA_CREATOR') || pool.hooks.toLowerCase().includes('ZORA_POST') &&
         chainId === ChainId.BASE
       if (isZoraPool) {
         if (pool.tvlETH <= 0.001) {
