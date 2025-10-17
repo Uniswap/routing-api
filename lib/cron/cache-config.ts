cb7b72fdafd389eb0f5c77c5271bd66f4235e6af
Alex <alex.francisci@gmail.com>
2025-10-17 16:14:52 -0400

    Fixing Random Comment Typos (#1235)
    * Adding typos from #1142
    
    * adding typos from #938
    
    * adding changes from #949

--- lib/cron/cache-config.ts
+++ lib/cron/cache-config.ts
@@ -160,7 +160,7 @@ const v3UntrackedUsdThreshold = 25000 // Pools need at least 25K USD (untracked)
 
 export const v2TrackedEthThreshold = 0.025 // Pairs need at least 0.025 of trackedEth to be selected
 export const v2BaseTrackedEthThreshold = 0.1 // Pairs on Base need at least 0.1 of trackedEth to be selected
-const v2UntrackedUsdThreshold = Number.MAX_VALUE // Pairs need at least 1K USD (untracked) to be selected (for metrics only)
+const v2UntrackedUsdThreshold = Number.MAX_VALUE // Pairs need untracked TVL higher than this value to be selected (for metrics only). Currently excludes all V2 pools with untracked TVL.
 
 export interface ChainProtocol {
   protocol: Protocol
