3eda2f6ea11a1adafacdaf7edf3506c31ad87dfd
hellno <hellno@users.noreply.github.com>
2026-01-14 03:04:25 +0100

    Add FARSTR hooks address to Base allowlist (#1229)
    Co-authored-by: Siyu Jiang (See-You John) <91580504+jsy1218@users.noreply.github.com>

--- lib/util/hooksAddressesAllowlist.ts
+++ lib/util/hooksAddressesAllowlist.ts
@@ -219,6 +219,9 @@ export const CLAUNCH_HOOK_ON_BASE = '0x2f9354bbb0edef5c2a5c4b78d0c59d73412a28cc'
 
 export const SEEDIFY_SPARK_HOOK_ON_BASE = '0x2fd54aaf84023eda60bd65edb5914c1a306850cc'
 
+// example pool: https://app.uniswap.org/explore/pools/base/0x6f104dafea59868dfee9883a56d666393633115c2fda5dca6da4aa6e39f18e2f
+export const FARSTR_HOOKS_ADDRESS_ON_BASE = '0xc3b8e77ac038aa260035a1911827086c34a9e844'
+
 // we do not allow v4 pools with non-zero hook address to be routed through in the initial v4 launch.
 // this is the ultimate safeguard in the routing subgraph pool cron job.
 export const HOOKS_ADDRESSES_ALLOWLIST: { [chain in ChainId]: Array<string> } = {
