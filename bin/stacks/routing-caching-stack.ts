1fdc95fe5a74dd24a35af3f7acdb8d34bac14019
Vasilis Xouris <vasileios.xouris@uniswap.org>
2026-03-19 18:00:39 +0200

    fix(subgraph): increase interval to 3h from 15m - used as fallback for now

--- bin/stacks/routing-caching-stack.ts
+++ bin/stacks/routing-caching-stack.ts
@@ -404,7 +404,7 @@ export class RoutingCachingStack extends cdk.NestedStack {
         }
       )
       new aws_events.Rule(this, `SchedulePoolCache-ChainId${chainId}-Protocol${protocol}`, {
-        schedule: aws_events.Schedule.rate(Duration.minutes(15)),
+        schedule: aws_events.Schedule.rate(Duration.hours(3)),
         targets: [new aws_events_targets.LambdaFunction(lambda)],
       })
       this.poolCacheBucket2.grantReadWrite(lambda)
